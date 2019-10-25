09c5f0841de62c5c7a349256d48cf089a9d3739c
goat <fakecake@icloud.com>
2019-10-26 02:11:51 +0900

    Use 64bit bitmask for CustomComboPreset

--- Dalamud/Game/ClientState/Structs/JobGauge/JobEnums.cs
+++ Dalamud/Game/ClientState/Structs/JobGauge/JobEnums.cs
@@ -1,25 +1,36 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dalamud.Game.ClientState.Structs.JobGauge {
-
     public enum SealType : byte {
-        NONE = 0, SUN, MOON, CELESTIAL
+        NONE = 0,
+        SUN,
+        MOON,
+        CELESTIAL
     }
+
     public enum CardType : byte {
-        NONE = 0, BALANCE, BOLE, ARROW, SPEAR, EWER, SPIRE,
-        LORD = 0x70, LADY = 0x80
+        NONE = 0,
+        BALANCE,
+        BOLE,
+        ARROW,
+        SPEAR,
+        EWER,
+        SPIRE,
+        LORD = 0x70,
+        LADY = 0x80
     }
 
     public enum SummonPet : byte {
-        IFRIT = 3, TITAN, GARUDA
+        IFRIT = 3,
+        TITAN,
+        GARUDA
     }
 
     public enum PetGlam : byte {
-        NONE = 0, EMERALD, TOPAZ, RUBY
+        NONE = 0,
+        EMERALD,
+        TOPAZ,
+        RUBY
     }
 
     [Flags]
@@ -27,22 +38,29 @@ namespace Dalamud.Game.ClientState.Structs.JobGauge {
         NONE = 0,
         SETSU = 1 << 0,
         GETSU = 1 << 1,
-        KA = 1 << 2,
+        KA = 1 << 2
     }
 
     public enum BOTDState : byte {
-        NONE = 0, BOTD, LOTD
+        NONE = 0,
+        BOTD,
+        LOTD
     }
 
     public enum CurrentSong : byte {
-        MAGE = 5, ARMY = 0xA, WANDERER = 0xF
+        MAGE = 5,
+        ARMY = 0xA,
+        WANDERER = 0xF
     }
 
     public enum DismissedFairy : byte {
-        EOS = 6, SELENE
+        EOS = 6,
+        SELENE
     }
 
     public enum Mudras : byte {
-        TEN = 1, CHI = 2, JIN = 3
+        TEN = 1,
+        CHI = 2,
+        JIN = 3
     }
 }
