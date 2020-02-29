d663252ed10455465ed6aea6ddf9cb5665730fcf
Mino <1381835+Minoost@users.noreply.github.com>
2020-02-29 18:40:53 +0900

    ntdll

--- Dalamud.Injector/OS/Process.cs
+++ Dalamud.Injector/OS/Process.cs
@@ -1,10 +1,33 @@
-﻿using System;
+using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Dalamud.Injector.OS
 {
-    internal sealed class Process
+    internal sealed partial class Process : IDisposable
     {
+        private SafeProcessHandle m_handle;
+    }
+
+    internal sealed partial class Process {
+        internal Process(SafeProcessHandle handle)
+        {
+            m_handle = handle;
+        }
+        
+        public static Process Open(uint pid/* and perms? maybe? */)
+        {
+            //
+            throw new NotImplementedException("TODO");
+        }
+
+        
+
+        public void Dispose()
+        {
+            m_handle?.Dispose();
+            m_handle = null!;
+        }
     }
 }
