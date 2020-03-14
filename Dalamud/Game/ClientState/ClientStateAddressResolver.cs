39e196ddbb7c2697760fab66bc2da877832bdf14
attickdoor <attickdoor@hotmail.com>
2020-03-13 23:31:11 -0400

    Add GetStaticAddressFromSig

--- Dalamud/Game/ClientState/ClientStateAddressResolver.cs
+++ Dalamud/Game/ClientState/ClientStateAddressResolver.cs
@@ -13,9 +13,9 @@ namespace Dalamud.Game.ClientState
         public IntPtr JobGaugeData { get; set; }
         
         protected override void Setup64Bit(SigScanner sig) {
-            ActorTable = sig.Module.BaseAddress + 0x1C62198;        // updated 5.21
+            ActorTable = sig.GetStaticAddressFromSig("F3 0F 11 05 ?? ?? ?? ?? EB 27", 0) + 0xC;
             LocalContentId = sig.Module.BaseAddress + 0x1C2E000;
-            JobGaugeData = sig.Module.BaseAddress + 0x1C5E420;
+            JobGaugeData = sig.GetStaticAddressFromSig("E8 ?? ?? ?? ?? 80 BB ?? ?? ?? ?? ?? 77 93", 0x220) + 0x10;
         }
     }
 }
