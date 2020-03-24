5f7d66f7346c5f9767d2cd335b7c53c4f04007b4
goat <fakecake@icloud.com>
2020-03-25 02:31:56 +0900

    fix: better territype sig

--- Dalamud/Game/ClientState/ClientStateAddressResolver.cs
+++ Dalamud/Game/ClientState/ClientStateAddressResolver.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using Dalamud.Game.Internal;
 
 namespace Dalamud.Game.ClientState
@@ -18,7 +13,7 @@ namespace Dalamud.Game.ClientState
             ActorTable = sig.GetStaticAddressFromSig("48 8D 0D ?? ?? ?? ?? 85 ED", 0) + 0x148;
             LocalContentId = sig.Module.BaseAddress + 0x1C2E000;
             JobGaugeData = sig.GetStaticAddressFromSig("E8 ?? ?? ?? ?? FF C6 48 8D 5B 0C", 0xB9) + 0x10;
-            TerritoryType = sig.GetStaticAddressFromSig("E8 ?? ?? ?? ?? 0F B6 5F 12 8B C8", 0x26);
+            TerritoryType = sig.GetStaticAddressFromSig("48 83 EC 20 81 3D ?? ?? ?? ?? ?? ?? ?? ?? 49 8B F8",0 );
         }
     }
 }
