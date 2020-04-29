84d1494b63594e225351757a9da373431681a4be
goat <16760685+goaaats@users.noreply.github.com>
2020-04-29 15:15:41 +0200

    feat: add implicit conversions to common vector types to Position3

--- Dalamud/Game/ClientState/Actors/Position3.cs
+++ Dalamud/Game/ClientState/Actors/Position3.cs
@@ -6,5 +6,17 @@ namespace Dalamud.Game.ClientState.Actors {
         public float X;
         public float Z;
         public float Y;
+
+        /// <summary>
+        /// Convert this Position3 to a System.Numerics.Vector3
+        /// </summary>
+        /// <param name="pos">Position to convert.</param>
+        public static implicit operator System.Numerics.Vector3(Position3 pos) => new System.Numerics.Vector3(pos.X, pos.Y, pos.Z);
+
+        /// <summary>
+        /// Convert this Position3 to a SharpDX.Vector3
+        /// </summary>
+        /// <param name="pos">Position to convert.</param>
+        public static implicit operator SharpDX.Vector3(Position3 pos) => new SharpDX.Vector3(pos.X, pos.Y, pos.Z);
     }
 }
