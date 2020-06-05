2f180d7c7db5dea39946e044e9e3e58c8a925803
goat <16760685+goaaats@users.noreply.github.com>
2020-06-05 14:36:51 +0200

    merge part 2

--- Dalamud/Dalamud.cs
+++ Dalamud/Dalamud.cs
@@ -151,8 +151,6 @@ namespace Dalamud {
 
                 IsReady = true;
             });
-            
-            this.conditionDebugWindow = new ConditionDebugWindow( this );
         }
 
         public void Start() {
@@ -218,7 +216,6 @@ namespace Dalamud {
         private DalamudCreditsWindow creditsWindow;
         private DalamudSettingsWindow settingsWindow;
         private PluginInstallerWindow pluginWindow;
-        private ConditionDebugWindow conditionDebugWindow;
         private DalamudPluginStatWindow pluginStatWindow;
 
         private void BuildDalamudUi()
@@ -276,16 +273,6 @@ namespace Dalamud {
                         ImGui.MenuItem(this.assemblyVersion, false);
                         ImGui.MenuItem(this.StartInfo.GameVersion, false);
 
-                        ImGui.EndMenu();
-                    }
-
-                    if( ImGui.BeginMenu( "Game" ) )
-                    {
-                        if( ImGui.MenuItem( "Condition Debug" ) )
-                        {
-                            this.conditionDebugWindow.Enabled = !this.conditionDebugWindow.Enabled;
-                        }
-                        
                         ImGui.EndMenu();
                     }
 
@@ -399,11 +386,6 @@ namespace Dalamud {
 
             if (this.isImguiDrawDemoWindow)
                 ImGui.ShowDemoWindow();
-
-            if( this.conditionDebugWindow.Enabled )
-            {
-                this.conditionDebugWindow.Draw();
-            }
 
             if (this.isImguiDrawPluginStatWindow) {
                 this.isImguiDrawPluginStatWindow = this.pluginStatWindow != null && this.pluginStatWindow.Draw();
