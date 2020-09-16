105ee5525813915653a22e3d1ab811807b6eca8c
Cara <git@caraxian.com>
2020-09-16 15:02:20 +0930

    Add "GetUiObjectByName"

--- Dalamud/Game/Internal/Gui/GameGuiAddressResolver.cs
+++ Dalamud/Game/Internal/Gui/GameGuiAddressResolver.cs
@@ -15,6 +15,8 @@ namespace Dalamud.Game.Internal.Gui {
         public IntPtr GetMatrixSingleton { get; private set; }
         public IntPtr ScreenToWorld { get; private set; }
         public IntPtr ToggleUiHide { get; set; }
+        public IntPtr GetBaseUIObject { get; private set; }
+        public IntPtr GetUIObjectByName { get; private set; }
 
         public GameGuiAddressResolver(IntPtr baseAddress) {
             BaseAddress = baseAddress;
@@ -37,6 +39,8 @@ namespace Dalamud.Game.Internal.Gui {
             GetMatrixSingleton = sig.ScanText("E8 ?? ?? ?? ?? 48 8D 4C 24 ?? 48 89 4c 24 ?? 4C 8D 4D ?? 4C 8D 44 24 ??");
             ScreenToWorld = sig.ScanText("48 83 EC 48 48 8B 05 ?? ?? ?? ?? 4D 8B D1");
             ToggleUiHide = sig.ScanText("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 0F B6 B9 ?? ?? ?? ?? B8 ?? ?? ?? ??");
+            GetBaseUIObject = sig.ScanText("E8 ?? ?? ?? ?? 41 B8 01 00 00 00 48 8D 15 ?? ?? ?? ?? 48 8B 48 20 E8 ?? ?? ?? ?? 48 8B CF");
+            GetUIObjectByName = sig.ScanText("E8 ?? ?? ?? ?? 48 8B CF 48 89 87 ?? ?? 00 00 E8 ?? ?? ?? ?? 41 B8 01 00 00 00");
         }
     }
 }
