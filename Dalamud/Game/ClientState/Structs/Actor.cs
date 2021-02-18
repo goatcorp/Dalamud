e2b2cadf04741563816c7cfeefeea7d6068e7c4d
Cara <git@caraxian.com>
2021-02-19 05:26:52 +1030

    Actor name as UTF8 string

--- Dalamud/Game/ClientState/Structs/Actor.cs
+++ Dalamud/Game/ClientState/Structs/Actor.cs
@@ -70,7 +70,7 @@ namespace Dalamud.Game.ClientState.Structs
     [StructLayout(LayoutKind.Explicit)]
     public struct Actor
     {
-        [FieldOffset(ActorOffsets.Name)] [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 30)] public string Name;
+        [FieldOffset(ActorOffsets.Name)] [MarshalAs(UnmanagedType.LPUTF8Str, SizeConst = 30)] public string Name;
 
         [FieldOffset(ActorOffsets.ActorId)] public int ActorId;
         [FieldOffset(ActorOffsets.DataId)] public int DataId;
