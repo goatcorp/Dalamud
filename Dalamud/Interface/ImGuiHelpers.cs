efdf9ca1eecbd80b5cc1bf0a421fe20e84f91809
goat <16760685+goaaats@users.noreply.github.com>
2021-04-05 17:36:45 +0200

    feat: cache viewport ID in ImGuiHelpers

--- Dalamud/Interface/ImGuiHelpers.cs
+++ Dalamud/Interface/ImGuiHelpers.cs
@@ -7,9 +7,23 @@ namespace Dalamud.Interface
     /// </summary>
     public static class ImGuiHelpers
     {
+        private static uint mainViewportId;
+
         /// <summary>
         /// Force this ImGui window to stay inside the main game window.
         /// </summary>
-        public static void ForceMainWindow() => ImGui.SetNextWindowViewport(ImGui.GetMainViewport().ID);
+        public static void ForceMainWindow() => ImGui.SetNextWindowViewport(GetMainViewportId());
+
+        /// <summary>
+        /// Get the ID of the main game window viewport.
+        /// </summary>
+        /// <returns>The ID of the main game window viewport.</returns>
+        public static uint GetMainViewportId()
+        {
+            if (mainViewportId == 0)
+                mainViewportId = ImGui.GetMainViewport().ID;
+
+            return mainViewportId;
+        }
     }
 }
