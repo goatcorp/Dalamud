1c66c2a3de63ed964f8467b4f3fcc4513a5778dc
kalilistic <35899782+kalilistic@users.noreply.github.com>
2021-04-09 23:29:08 -0400

    refactor: update custom imgui component setup

---
+++ Dalamud/Interface/Components/ImGuiComponents.Test.cs
@@ -0,0 +1,18 @@
+using ImGuiNET;
+
+namespace Dalamud.Interface.Components
+{
+    /// <summary>
+    /// Class containing various methods providing ImGui components.
+    /// </summary>
+    public static partial class ImGuiComponents
+    {
+        /// <summary>
+        /// Test component to demonstrate how ImGui components work.
+        /// </summary>
+        public static void Test()
+        {
+            ImGui.Text("You are viewing the test component. The test was a success.");
+        }
+    }
+}
