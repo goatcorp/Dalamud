bf1b8b06e6d4aa567d003c97f1a0cdae4b802931
goat <16760685+goaaats@users.noreply.github.com>
2021-07-18 19:32:10 +0200

    feat(Animation): add InOutSine

---
+++ Dalamud/Interface/Animation/EasingFunctions/InOutSine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Dalamud.Interface.Animation.EasingFunctions
+{
+    /// <summary>
+    /// Class providing an "InOutSine" easing animation.
+    /// </summary>
+    public class InOutSine : Easing
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InOutSine"/> class.
+        /// </summary>
+        /// <param name="duration">The duration of the animation.</param>
+        public InOutSine(TimeSpan duration)
+            : base(duration)
+        {
+            // ignored
+        }
+
+        /// <inheritdoc/>
+        public override void Update()
+        {
+            var p = this.Progress;
+            this.Value = -(Math.Cos(Math.PI * p) - 1) / 2;
+        }
+    }
+}
