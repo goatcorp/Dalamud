3ca6f5e9428cfa6d97ca7ed26d601fb96819e0dd
goat <16760685+goaaats@users.noreply.github.com>
2021-07-18 19:31:52 +0200

    feat(Animation): add InOutCirc

---
+++ Dalamud/Interface/Animation/EasingFunctions/InOutCirc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dalamud.Interface.Animation.EasingFunctions
+{
+    /// <summary>
+    /// Class providing an "InOutCirc" easing animation.
+    /// </summary>
+    public class InOutCirc : Easing
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InOutCirc"/> class.
+        /// </summary>
+        /// <param name="duration">The duration of the animation.</param>
+        public InOutCirc(TimeSpan duration)
+            : base(duration)
+        {
+            // ignored
+        }
+
+        /// <inheritdoc/>
+        public override void Update()
+        {
+            var p = this.Progress;
+            this.Value = p < 0.5
+                             ? (1 - Math.Sqrt(1 - Math.Pow(2 * p, 2))) / 2
+                             : (Math.Sqrt(1 - Math.Pow((-2 * p) + 2, 2)) + 1) / 2;
+        }
+    }
+}
