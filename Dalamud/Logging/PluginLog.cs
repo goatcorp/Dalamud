c699d403cdf3c2753631dd35d315d05975fdfead
Raymond <daemitus@gmail.com>
2021-08-09 17:40:05 -0400

    Restore PluginLog.LogN methods

--- Dalamud/Logging/PluginLog.cs
+++ Dalamud/Logging/PluginLog.cs
@@ -8,6 +8,131 @@ namespace Dalamud.Logging
     /// </summary>
     public static class PluginLog
     {
+        #region "Log" prefixed Serilog style methods
+
+        /// <summary>
+        /// Log a templated message to the in-game debug log.
+        /// </summary>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void Log(string messageTemplate, params object[] values)
+            => Serilog.Log.Information($"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated message to the in-game debug log.
+        /// </summary>
+        /// <param name="exception">The exception that caused the error.</param>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void Log(Exception exception, string messageTemplate, params object[] values)
+            => Serilog.Log.Information(exception, $"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated verbose message to the in-game debug log.
+        /// </summary>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogVerbose(string messageTemplate, params object[] values)
+            => Serilog.Log.Verbose($"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated verbose message to the in-game debug log.
+        /// </summary>
+        /// <param name="exception">The exception that caused the error.</param>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogVerbose(Exception exception, string messageTemplate, params object[] values)
+            => Serilog.Log.Verbose(exception, $"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated debug message to the in-game debug log.
+        /// </summary>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogDebug(string messageTemplate, params object[] values)
+            => Serilog.Log.Debug($"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated debug message to the in-game debug log.
+        /// </summary>
+        /// <param name="exception">The exception that caused the error.</param>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogDebug(Exception exception, string messageTemplate, params object[] values)
+            => Serilog.Log.Debug(exception, $"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated information message to the in-game debug log.
+        /// </summary>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogInformation(string messageTemplate, params object[] values)
+            => Serilog.Log.Information($"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated information message to the in-game debug log.
+        /// </summary>
+        /// <param name="exception">The exception that caused the error.</param>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogInformation(Exception exception, string messageTemplate, params object[] values)
+            => Serilog.Log.Information(exception, $"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated warning message to the in-game debug log.
+        /// </summary>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogWarning(string messageTemplate, params object[] values)
+            => Serilog.Log.Warning($"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated warning message to the in-game debug log.
+        /// </summary>
+        /// <param name="exception">The exception that caused the error.</param>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogWarning(Exception exception, string messageTemplate, params object[] values)
+            => Serilog.Log.Warning(exception, $"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated error message to the in-game debug log.
+        /// </summary>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogError(string messageTemplate, params object[] values)
+            => Serilog.Log.Error($"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated error message to the in-game debug log.
+        /// </summary>
+        /// <param name="exception">The exception that caused the error.</param>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogError(Exception exception, string messageTemplate, params object[] values)
+            => Serilog.Log.Error(exception, $"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated fatal message to the in-game debug log.
+        /// </summary>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogFatal(string messageTemplate, params object[] values)
+            => Serilog.Log.Fatal($"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        /// <summary>
+        /// Log a templated fatal message to the in-game debug log.
+        /// </summary>
+        /// <param name="exception">The exception that caused the error.</param>
+        /// <param name="messageTemplate">The message template.</param>
+        /// <param name="values">Values to log.</param>
+        public static void LogFatal(Exception exception, string messageTemplate, params object[] values)
+            => Serilog.Log.Fatal(exception, $"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        #endregion
+
+        #region Serilog style methods
+
         /// <summary>
         /// Log a templated verbose message to the in-game debug log.
         /// </summary>
@@ -109,5 +234,7 @@ namespace Dalamud.Logging
         /// <param name="values">Values to log.</param>
         public static void Fatal(Exception exception, string messageTemplate, params object[] values)
             => Serilog.Log.Fatal(exception, $"[{Assembly.GetCallingAssembly().GetName().Name}] {messageTemplate}", values);
+
+        #endregion
     }
 }
