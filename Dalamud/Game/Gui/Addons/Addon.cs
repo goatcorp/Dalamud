ee99fe4499535e7744a9b05623685defb48617b9
Raymond <daemitus@gmail.com>
2021-08-09 18:45:55 -0400

    Live addon rework

--- Dalamud/Game/Gui/Addons/Addon.cs
+++ Dalamud/Game/Gui/Addons/Addon.cs
@@ -1,66 +1,63 @@
 using System;
 
-namespace Dalamud.Game.Internal.Gui.Addon
+using Dalamud.Memory;
+
+namespace Dalamud.Game.Gui.Addons
 {
     /// <summary>
     /// This class represents an in-game UI "Addon".
     /// </summary>
-    public class Addon
+    public unsafe class Addon
     {
-        /// <summary>
-        /// The address of the addon.
-        /// </summary>
-        public IntPtr Address;
-
-        /// <summary>
-        /// The addon interop data.
-        /// </summary>
-        protected Structs.Addon addonStruct;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Addon"/> class.
         /// </summary>
         /// <param name="address">The address of the addon.</param>
-        /// <param name="addonStruct">The addon interop data.</param>
-        public Addon(IntPtr address, Structs.Addon addonStruct)
+        public Addon(IntPtr address)
         {
             this.Address = address;
-            this.addonStruct = addonStruct;
         }
+
+        /// <summary>
+        /// Gets the address of the addon.
+        /// </summary>
+        public IntPtr Address { get; }
 
         /// <summary>
         /// Gets the name of the addon.
         /// </summary>
-        public string Name => this.addonStruct.Name;
+        public string Name => MemoryHelper.ReadString((IntPtr)this.Struct->Name, 0x20);
 
         /// <summary>
         /// Gets the X position of the addon on screen.
         /// </summary>
-        public short X => this.addonStruct.X;
+        public short X => this.Struct->X;
 
         /// <summary>
         /// Gets the Y position of the addon on screen.
         /// </summary>
-        public short Y => this.addonStruct.Y;
+        public short Y => this.Struct->Y;
 
         /// <summary>
         /// Gets the scale of the addon.
         /// </summary>
-        public float Scale => this.addonStruct.Scale;
+        public float Scale => this.Struct->Scale;
 
         /// <summary>
         /// Gets the width of the addon. This may include non-visible parts.
         /// </summary>
-        public unsafe float Width => this.addonStruct.RootNode->Width * this.Scale;
+        public unsafe float Width => this.Struct->RootNode->Width * this.Scale;
 
         /// <summary>
         /// Gets the height of the addon. This may include non-visible parts.
         /// </summary>
-        public unsafe float Height => this.addonStruct.RootNode->Height * this.Scale;
+        public unsafe float Height => this.Struct->RootNode->Height * this.Scale;
 
         /// <summary>
         /// Gets a value indicating whether the addon is visible.
         /// </summary>
-        public bool Visible => (this.addonStruct.Flags & 0x20) == 0x20;
+        public bool Visible => this.Struct->IsVisible;
+
+        private FFXIVClientStructs.FFXIV.Component.GUI.AtkUnitBase* Struct => (FFXIVClientStructs.FFXIV.Component.GUI.AtkUnitBase*)this.Address;
     }
 }
