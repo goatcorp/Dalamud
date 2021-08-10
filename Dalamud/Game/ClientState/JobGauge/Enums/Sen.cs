a5cccf8a763179f3c49642cc9df5d5f3eb494722
Raymond <daemitus@gmail.com>
2021-08-10 17:32:03 -0400

    Job Gauge update

---
+++ Dalamud/Game/ClientState/JobGauge/Enums/Sen.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Dalamud.Game.ClientState.JobGauge.Enums
+{
+    /// <summary>
+    /// Samurai Sen types.
+    /// </summary>
+    [Flags]
+    public enum Sen : byte
+    {
+        /// <summary>
+        /// No Sen.
+        /// </summary>
+        NONE = 0,
+
+        /// <summary>
+        /// Setsu Sen type.
+        /// </summary>
+        SETSU = 1 << 0,
+
+        /// <summary>
+        /// Getsu Sen type.
+        /// </summary>
+        GETSU = 1 << 1,
+
+        /// <summary>
+        /// Ka Sen type.
+        /// </summary>
+        KA = 1 << 2,
+    }
+}
