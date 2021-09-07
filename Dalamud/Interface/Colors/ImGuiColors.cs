de83a799ae50dbe7de015cceb7d2280874f27625
Raymond <daemitus@gmail.com>
2021-09-07 10:55:40 -0400

    formatting

--- Dalamud/Interface/Colors/ImGuiColors.cs
+++ Dalamud/Interface/Colors/ImGuiColors.cs
@@ -15,17 +15,17 @@ namespace Dalamud.Interface.Colors
         /// <summary>
         /// Gets grey used in dalamud.
         /// </summary>
-        public static Vector4 DalamudGrey { get; } = new Vector4(0.70f, 0.70f, 0.70f, 1.00f);
+        public static Vector4 DalamudGrey { get; } = new Vector4(0.7f, 0.7f, 0.7f, 1f);
 
         /// <summary>
         /// Gets grey used in dalamud.
         /// </summary>
-        public static Vector4 DalamudGrey2 { get; } = new Vector4(0.7f, 0.7f, 0.7f, 1.0f);
+        public static Vector4 DalamudGrey2 { get; } = new Vector4(0.7f, 0.7f, 0.7f, 1f);
 
         /// <summary>
         /// Gets grey used in dalamud.
         /// </summary>
-        public static Vector4 DalamudGrey3 { get; } = new Vector4(0.5f, 0.5f, 0.5f, 1.0f);
+        public static Vector4 DalamudGrey3 { get; } = new Vector4(0.5f, 0.5f, 0.5f, 1f);
 
         /// <summary>
         /// Gets white used in dalamud.
@@ -45,16 +45,16 @@ namespace Dalamud.Interface.Colors
         /// <summary>
         /// Gets tank blue (UIColor37).
         /// </summary>
-        public static Vector4 TankBlue { get; } = new Vector4(0, 0.6f, 1, 1);
+        public static Vector4 TankBlue { get; } = new Vector4(0f, 0.6f, 1f, 1f);
 
         /// <summary>
         /// Gets healer green (UIColor504).
         /// </summary>
-        public static Vector4 HealerGreen { get; } = new Vector4(0, 0.8f, 0.1333333f, 1);
+        public static Vector4 HealerGreen { get; } = new Vector4(0f, 0.8f, 0.1333333f, 1f);
 
         /// <summary>
         /// Gets dps red (UIColor545).
         /// </summary>
-        public static Vector4 DPSRed { get; } = new Vector4(0.7058824f, 0, 0, 1);
+        public static Vector4 DPSRed { get; } = new Vector4(0.7058824f, 0f, 0f, 1f);
     }
 }
