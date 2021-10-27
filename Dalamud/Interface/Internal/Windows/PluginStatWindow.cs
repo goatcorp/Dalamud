8b88bc18e0cda996927af3e4bbb9495dc1bc5d99
goat <16760685+goaaats@users.noreply.github.com>
2021-10-28 01:39:08 +0200

    correct table setup

--- Dalamud/Interface/Internal/Windows/PluginStatWindow.cs
+++ Dalamud/Interface/Internal/Windows/PluginStatWindow.cs
@@ -176,11 +176,12 @@ namespace Dalamud.Interface.Internal.Windows
 
             if (ImGui.BeginTabItem("Hooks"))
             {
-                ImGui.Columns(3);
+                ImGui.Columns(4);
 
-                ImGui.SetColumnWidth(0, ImGui.GetWindowContentRegionWidth() - 280);
+                ImGui.SetColumnWidth(0, ImGui.GetWindowContentRegionWidth() - 330);
                 ImGui.SetColumnWidth(1, 180f);
                 ImGui.SetColumnWidth(2, 100f);
+                ImGui.SetColumnWidth(3, 100f);
 
                 ImGui.Text("Detour Method");
                 ImGui.SameLine();
