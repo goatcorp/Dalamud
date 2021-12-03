8208a62941d22e88c67d0550004c0c3bb0921ad5
Raymond <daemitus@gmail.com>
2021-12-02 21:53:09 -0500

    fix sig: SetupTerritoryType

--- Dalamud/Game/ClientState/ClientStateAddressResolver.cs
+++ Dalamud/Game/ClientState/ClientStateAddressResolver.cs
@@ -96,7 +96,7 @@ namespace Dalamud.Game.ClientState
             this.LocalContentId = sig.GetStaticAddressFromSig("48 0F 44 05 ?? ?? ?? ?? 48 39 07");
             this.JobGaugeData = sig.GetStaticAddressFromSig("48 8B 3D ?? ?? ?? ?? 48 85 FF 0F 84 ?? ?? ?? ?? 0F B6 05 ?? ?? ?? ??") + 0x8;
 
-            this.SetupTerritoryType = sig.ScanText("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 48 8B F9 66 89 91 ?? ?? ?? ??");
+            this.SetupTerritoryType = sig.ScanText("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 48 8B F9 66 89 91 ?? ?? ?? ??");
 
             // These resolve to fixed offsets only, without the base address added in, so GetStaticAddressFromSig() can't be used.
             // lea   rcx, ds:1DB9F74h[rax*4]          KeyboardState
