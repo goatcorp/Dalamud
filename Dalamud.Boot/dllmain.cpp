04bf21af78ed518bed11472ed41a8b74b9ba86aa
goaaats <16760685+goaaats@users.noreply.github.com>
2021-12-23 15:39:37 +0100

    chore: turn VEH back on, add DALAMUD_NO_VEH env var

--- Dalamud.Boot/dllmain.cpp
+++ Dalamud.Boot/dllmain.cpp
@@ -73,11 +73,10 @@ DllExport DWORD WINAPI Initialize(LPVOID lpParam)
 
     // ============================== VEH ======================================== //
 
-    /*
     printf("Initializing VEH... ");
-    if(is_running_on_linux())
+    if(is_running_on_linux() || getenv("DALAMUD_NO_VEH"))
     {
-        printf("Failed! [Disabled for Wine]\n");
+        printf("VEH was disabled manually!\n");
     }
     else
     {
@@ -85,7 +84,6 @@ DllExport DWORD WINAPI Initialize(LPVOID lpParam)
             printf("Done!\n");
         else printf("Failed!\n");
     }
-    */
 
     // =========================================================================== //
 
@@ -108,7 +106,7 @@ BOOL APIENTRY DllMain(const HMODULE hModule, const DWORD dwReason, LPVOID lpRese
             g_hModule = hModule;
             break;
         case DLL_PROCESS_DETACH:
-            //veh::remove_handler();
+            veh::remove_handler();
             break;
     }
     return TRUE;
