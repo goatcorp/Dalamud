440ec7433ce3088b0dc21d2feb85ca183bb1129e
goaaats <goatsdev@protonmail.com>
2022-01-29 15:59:18 +0100

    deps: update FFXIVClientStructs

--- Dalamud/Game/Gui/GameGui.cs
+++ Dalamud/Game/Gui/GameGui.cs
@@ -412,7 +412,7 @@ namespace Dalamud.Game.Gui
             var unitBase = (FFXIVClientStructs.FFXIV.Component.GUI.AtkUnitBase*)addon;
             var id = unitBase->ParentID;
             if (id == 0)
-                id = unitBase->ID;
+                id = unitBase->IDu;
 
             if (id == 0)
                 return IntPtr.Zero;
