5eae9c23a811aab2a20b94c89812947bd297993c
Soreepeong <soreepeong@gmail.com>
2022-04-25 10:32:30 +0900

    CoreCLR: resolve and load nethost on demand instead of requiring it on load

--- Dalamud.Injector.Boot/main.cpp
+++ Dalamud.Injector.Boot/main.cpp
@@ -21,6 +21,7 @@ int wmain(int argc, wchar_t** argv)
 
     void* entrypoint_vfn;
     int result = InitializeClrAndGetEntryPoint(
+        GetModuleHandleW(nullptr),
         runtimeconfig_path,
         module_path,
         L"Dalamud.Injector.EntryPoint, Dalamud.Injector",
