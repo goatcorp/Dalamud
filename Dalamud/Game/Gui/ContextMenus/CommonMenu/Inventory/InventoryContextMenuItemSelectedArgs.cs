d23f1a5984822f1bbbdc6fbe1fc617784bb75d5e
kalilistic <35899782+kalilistic@users.noreply.github.com>
2022-04-25 21:06:47 -0400

    chore: copy in common context menu

---
+++ Dalamud/Game/Gui/ContextMenus/CommonMenu/Inventory/InventoryContextMenuItemSelectedArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dalamud.Game.Gui.ContextMenus.CommonMenu.Inventory;
+
+/// <summary>
+/// The arguments for when an inventory context menu item is selected
+/// </summary>
+public class InventoryContextMenuItemSelectedArgs : BaseInventoryContextMenuArgs {
+    internal InventoryContextMenuItemSelectedArgs(IntPtr addon, IntPtr agent, string? parentAddonName, uint itemId, uint itemAmount, bool itemHq) : base(addon, agent, parentAddonName, itemId, itemAmount, itemHq) {
+    }
+}
---
+++ Dalamud/Game/Gui/ContextMenus/CommonMenu/Inventory/InventoryContextSubMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Dalamud.Game.Text.SeStringHandling;
+
+namespace Dalamud.Game.Gui.ContextMenus.CommonMenu.Inventory;
+
+/// <summary>
+/// A custom inventory context menu item that will open a submenu
+/// </summary>
+public class InventoryContextSubMenuItem : CustomContextMenuItem<ContextMenu.InventoryContextMenuOpenEventDelegate> {
+    /// <summary>
+    /// Create a new context menu item for inventory items that will open a submenu.
+    /// </summary>
+    /// <param name="name">the English name of the item, copied to other languages</param>
+    /// <param name="action">the action to perform on click</param>
+    [Obsolete("Inventory context submenus do not work yet", true)]
+    public InventoryContextSubMenuItem(SeString name, ContextMenu.InventoryContextMenuOpenEventDelegate action) : base(name, action) {
+    }
+}
