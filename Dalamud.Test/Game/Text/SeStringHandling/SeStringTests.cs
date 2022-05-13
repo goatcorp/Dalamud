2626315ad26988ef0a2292636b54db281360d5c7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-13 10:07:44 -0700

    chore: add SeString JSON conversion test

---
+++ Dalamud.Test/Game/Text/SeStringHandling/SeStringTests.cs
@@ -0,0 +1,18 @@
+﻿using Dalamud.Game.Text.SeStringHandling;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Dalamud.Test.Game.Text.SeStringHandling
+{
+    public class SeStringTests
+    {
+        // Dalamud#779
+        [Fact]
+        public void TestJsonSerializable()
+        {
+            var builder = new SeStringBuilder();
+            var seString = builder.AddText("Some text").Build();
+            JsonConvert.SerializeObject(seString);
+        }
+    }
+}
