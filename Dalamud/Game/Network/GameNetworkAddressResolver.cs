319879938fb01d9ace0c34434e41f45e28a7ed9f
Aireil <33433913+Aireil@users.noreply.github.com>
2022-07-12 00:04:32 +0200

    refactor: use a winter sig for cf pop packet

--- Dalamud/Game/Network/GameNetworkAddressResolver.cs
+++ Dalamud/Game/Network/GameNetworkAddressResolver.cs
@@ -29,7 +29,7 @@ namespace Dalamud.Game.Network
             // ProcessZonePacket = sig.ScanText("48 89 74 24 18 57 48 83  EC 50 8B F2 49 8B F8 41 0F B7 50 02 8B CE E8 ?? ?? 73 FF 0F B7 57 02 8D 42 ?? 3D ?? ?? 00 00 0F 87 60 01 00 00 4C 8D 05");
             this.ProcessZonePacketDown = sig.ScanText("48 89 5C 24 ?? 56 48 83 EC 50 8B F2");
             this.ProcessZonePacketUp = sig.ScanText("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 41 56 41 57 48 83 EC 70 8B 81 ?? ?? ?? ??");
-            this.ProcessCfPopPacket = sig.ScanText("40 53 57 48 83 EC 78 48 8B D9 48 8D 0D");
+            this.ProcessCfPopPacket = sig.ScanText("40 53 57 48 83 EC 78 48 8B D9 48 8D 0D ?? ?? ?? 00 E8 ?? ?? ?? FF");
         }
     }
 }
