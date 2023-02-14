28d88b112e1d714c646b379035321c80924547c0
PunishedPineapple <50609717+PunishedPineapple@users.noreply.github.com>
2023-02-13 20:21:11 -0600

    Implemented the chat type localization.  It's not great, but the best I can do without further ruffling feathers.

--- Dalamud/Game/Text/XivChatType2Extensions.cs
+++ Dalamud/Game/Text/XivChatType2Extensions.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using CheapLoc;
+
 using Dalamud.Utility;
 
 namespace Dalamud.Game.Text;
@@ -77,8 +79,102 @@ public static class XivChatType2Extensions
     /// <returns>A string containing the XivChatType's name.</returns>
     public static string GetTranslatedName(this XivChatType2 chatType)
     {
-        // ***** TODO: Stubbed for now.
-        return $"{chatType}";
+        // This isn't great, but right now, we can't do anything nicer without objectionable changes.
+        return chatType switch
+        {
+            XivChatType2.None => Loc.Localize("XivChatType.None", "None"),
+            XivChatType2.Debug => Loc.Localize("XivChatType.Debug", "Debug"),
+            XivChatType2.Urgent => Loc.Localize("XivChatType.Urgent", "Urgent"),
+            XivChatType2.Notice => Loc.Localize("XivChatType.Notice", "Notice"),
+            XivChatType2.Say => Loc.Localize("XivChatType.Say", "Say"),
+            XivChatType2.Shout => Loc.Localize("XivChatType.Shout", "Shout"),
+            XivChatType2.TellOutgoing => Loc.Localize("XivChatType.TellOutgoing", "Tell (Outgoing)"),
+            XivChatType2.TellIncoming => Loc.Localize("XivChatType.TellIncoming", "Tell (Incoming)"),
+            XivChatType2.Party => Loc.Localize("XivChatType.Party", "Party"),
+            XivChatType2.Alliance => Loc.Localize("XivChatType.Alliance", "Alliance"),
+            XivChatType2.Ls1 => Loc.Localize("XivChatType.Ls1", "Linkshell 1"),
+            XivChatType2.Ls2 => Loc.Localize("XivChatType.Ls2", "Linkshell 2"),
+            XivChatType2.Ls3 => Loc.Localize("XivChatType.Ls3", "Linkshell 3"),
+            XivChatType2.Ls4 => Loc.Localize("XivChatType.Ls4", "Linkshell 4"),
+            XivChatType2.Ls5 => Loc.Localize("XivChatType.Ls5", "Linkshell 5"),
+            XivChatType2.Ls6 => Loc.Localize("XivChatType.Ls6", "Linkshell 6"),
+            XivChatType2.Ls7 => Loc.Localize("XivChatType.Ls7", "Linkshell 7"),
+            XivChatType2.Ls8 => Loc.Localize("XivChatType.Ls8", "Linkshell 8"),
+            XivChatType2.FreeCompany => Loc.Localize("XivChatType.FreeCompany", "Free Company"),
+            XivChatType2.NoviceNetwork => Loc.Localize("XivChatType.NoviceNetwork", "Novice Network"),
+            XivChatType2.CustomEmote => Loc.Localize("XivChatType.CustomEmote", "Custom Emote"),
+            XivChatType2.StandardEmote => Loc.Localize("XivChatType.StandardEmote", "Standard Emote"),
+            XivChatType2.Yell => Loc.Localize("XivChatType.Yell", "Yell"),
+            XivChatType2.CrossParty => Loc.Localize("XivChatType.CrossParty", "Cross-World Party"),
+            XivChatType2.PvPTeam => Loc.Localize("XivChatType.PvPTeam", "PvP Team"),
+            XivChatType2.CrossLinkShell1 => Loc.Localize("XivChatType.CrossLinkShell1", "Cross-World Linkshell 1"),
+            XivChatType2.BattleLog_Damage => Loc.Localize("XivChatType.BattleLog_Damage", "Battle Log (Damage)"),
+            XivChatType2.BattleLog_Miss => Loc.Localize("XivChatType.BattleLog_Miss", "Battle Log (Misses)"),
+            XivChatType2.BattleLog_Action => Loc.Localize("XivChatType.BattleLog_Action", "Battle Log (Actions)"),
+            XivChatType2.BattleLog_Item => Loc.Localize("XivChatType.BattleLog_Item", "Battle Log (Items)"),
+            XivChatType2.BattleLog_Healing => Loc.Localize("XivChatType.BattleLog_Healing", "Battle Log (Healing)"),
+            XivChatType2.BattleLog_Buff => Loc.Localize("XivChatType.BattleLog_Buff", "Battle Log (Buffs)"),
+            XivChatType2.BattleLog_Debuff => Loc.Localize("XivChatType.BattleLog_Debuff", "Battle Log (Debuffs)"),
+            XivChatType2.BattleLog_BuffExpiration => Loc.Localize("XivChatType.BattleLog_BuffExpiration", "Battle Log (Buffs Expiring)"),
+            XivChatType2.BattleLog_DebuffExpiration => Loc.Localize("XivChatType.BattleLog_DebuffExpiration", "Battle Log (Debuffs Expiring)"),
+            XivChatType2.AlarmNotification => Loc.Localize("XivChatType.AlarmNotification", "Alarm Notifications"),
+            XivChatType2.Echo => Loc.Localize("XivChatType.Echo", "Echo"),
+            XivChatType2.SystemMessage => Loc.Localize("XivChatType.SystemMessage", "System Messages"),
+            XivChatType2.BattleSystemMessage => Loc.Localize("XivChatType.BattleSystemMessage", "Battle System Messages"),
+            XivChatType2.GatheringSystemMessage => Loc.Localize("XivChatType.GatheringSystemMessage", "Gathering System Messages"),
+            XivChatType2.ErrorMessage => Loc.Localize("XivChatType.ErrorMessage", "Error Messages"),
+            XivChatType2.NPCDialogue => Loc.Localize("XivChatType.NPCDialogue", "NPC Dialogue"),
+            XivChatType2.LootNotice => Loc.Localize("XivChatType.LootNotice", "Loot Notices"),
+            XivChatType2.ProgressionMessage => Loc.Localize("XivChatType.ProgressionMessage", "Progression Messages" ),
+            XivChatType2.LootMessage => Loc.Localize("XivChatType.LootMessage", "Loot Messages"),
+            XivChatType2.SynthesisMessage => Loc.Localize("XivChatType.SynthesisMessage", "Synthesis Messages"),
+            XivChatType2.GatheringMessage => Loc.Localize("XivChatType.GatheringMessage", "Gathering Messages"),
+            XivChatType2.NPCDialogueAnnouncements => Loc.Localize("XivChatType.NPCDialogueAnnouncements", "NPC Dialogue (Announcements)"),
+            XivChatType2.FreeCompanyAnnouncement => Loc.Localize("XivChatType.FreeCompanyAnnouncement", "Free Company Announcements"),
+            XivChatType2.FreeCompanyLogin => Loc.Localize("XivChatType.FreeCompanyLogin", "Free Company Logins"),
+            XivChatType2.RetainerSale => Loc.Localize("XivChatType.RetainerSale", "Retainer Sales"),
+            XivChatType2.RecruitmentNotification => Loc.Localize("XivChatType.RecruitmentNotification", "Recruitment Notifications"),
+            XivChatType2.SignMessage => Loc.Localize("XivChatType.SignMessage", "Sign Messages"),
+            XivChatType2.RandomNumberMessage => Loc.Localize("XivChatType.RandomNumberMessage", "Random Number Messages"),
+            XivChatType2.NoviceNetworkNotification => Loc.Localize("XivChatType.NoviceNetworkNotification", "Novice Network Notifications"),
+            XivChatType2.OrchestrionTrackMessage => Loc.Localize("XivChatType.OrchestrionTrackMessage", "Orchestrion Track Messages"),
+            XivChatType2.PvPTeamAnnouncement => Loc.Localize("XivChatType.PvPTeamAnnouncement", "PvP Team Announcements"),
+            XivChatType2.PvPTeamLogin => Loc.Localize("XivChatType.PvPTeamLogin", "PvP Team Logins"),
+            XivChatType2.MessageBookAlert => Loc.Localize("XivChatType.MessageBookAlert", "Message Book Alerts"),
+            XivChatType2.CrossLinkShell2 => Loc.Localize("XivChatType.CrossLinkShell2", "Cross-World Linkshell 2"),
+            XivChatType2.CrossLinkShell3 => Loc.Localize("XivChatType.CrossLinkShell3", "Cross-World Linkshell 3"),
+            XivChatType2.CrossLinkShell4 => Loc.Localize("XivChatType.CrossLinkShell4", "Cross-World Linkshell 4"),
+            XivChatType2.CrossLinkShell5 => Loc.Localize("XivChatType.CrossLinkShell5", "Cross-World Linkshell 5"),
+            XivChatType2.CrossLinkShell6 => Loc.Localize("XivChatType.CrossLinkShell6", "Cross-World Linkshell 6"),
+            XivChatType2.CrossLinkShell7 => Loc.Localize("XivChatType.CrossLinkShell7", "Cross-World Linkshell 7"),
+            XivChatType2.CrossLinkShell8 => Loc.Localize("XivChatType.CrossLinkShell8", "Cross-World Linkshell 8"),
+
+            XivChatType2.TargetMask_You => Loc.Localize("XivChatType.TargetMask_You", "Target Mask - You"),
+            XivChatType2.TargetMask_PartyMember => Loc.Localize("XivChatType.TargetMask_PartyMember", "Target Mask - Party Members"),
+            XivChatType2.TargetMask_AllianceMember => Loc.Localize("XivChatType.TargetMask_AllianceMember", "Target Mask - Alliance Members"),
+            XivChatType2.TargetMask_OtherPlayers => Loc.Localize("XivChatType.TargetMask_OtherPlayers", "Target Mask - Other Players"),
+            XivChatType2.TargetMask_EngagedEnemy => Loc.Localize("XivChatType.TargetMask_EngagedEnemy", "Target Mask - Engaged Enemies"),
+            XivChatType2.TargetMask_UnengagedEnemy => Loc.Localize("XivChatType.TargetMask_UnengagedEnemy", "Target Mask - Unengaged Enemies"),
+            XivChatType2.TargetMask_FriendlyNPC => Loc.Localize("XivChatType.TargetMask_FriendlyNPC", "Target Mask - Friendly NPCs"),
+            XivChatType2.TargetMask_YourPet => Loc.Localize("XivChatType.TargetMask_YourPet", "Target Mask - Your Pets/Companions"),
+            XivChatType2.TargetMask_PartyPet => Loc.Localize("XivChatType.TargetMask_PartyPet", "Target Mask - Party Members' Pets/Companions"),
+            XivChatType2.TargetMask_AlliancePet => Loc.Localize("XivChatType.TargetMask_AlliancePet", "Target Mask - Alliance Members' Pets/Companions"),
+            XivChatType2.TargetMask_OtherPlayerPet => Loc.Localize("XivChatType.TargetMask_OtherPlayerPet", "Target Mask - Other Players' Pets/Companions"),
+
+            XivChatType2.SourceMask_You => Loc.Localize("XivChatType.SourceMask_You", "Source Mask - You"),
+            XivChatType2.SourceMask_PartyMember => Loc.Localize("XivChatType.SourceMask_PartyMember", "Source Mask - Party Members"),
+            XivChatType2.SourceMask_AllianceMember => Loc.Localize("XivChatType.SourceMask_AllianceMember", "Source Mask - Alliance Members"),
+            XivChatType2.SourceMask_OtherPlayers => Loc.Localize("XivChatType.SourceMask_OtherPlayers", "Source Mask - Other Players"),
+            XivChatType2.SourceMask_EngagedEnemy => Loc.Localize("XivChatType.SourceMask_EngagedEnemy", "Source Mask - Engaged Enemies"),
+            XivChatType2.SourceMask_UnengagedEnemy => Loc.Localize("XivChatType.SourceMask_UnengagedEnemy", "Source Mask - Unengaged Enemies"),
+            XivChatType2.SourceMask_FriendlyNPC => Loc.Localize("XivChatType.SourceMask_FriendlyNPC", "Source Mask - Friendly NPCs"),
+            XivChatType2.SourceMask_YourPet => Loc.Localize("XivChatType.SourceMask_YourPet", "Source Mask - Your Pets/Companions"),
+            XivChatType2.SourceMask_PartyPet => Loc.Localize("XivChatType.SourceMask_PartyPet", "Source Mask - Party Members' Pets/Companions"),
+            XivChatType2.SourceMask_AlliancePet => Loc.Localize("XivChatType.SourceMask_AlliancePet", "Source Mask - Alliance Members' Pets/Companions"),
+            XivChatType2.SourceMask_OtherPlayerPet => Loc.Localize("XivChatType.SourceMask_OtherPlayerPet", "Source Mask - Other Players' Pets/Companions"),
+
+            _ => $"{chatType}",
+        };
     }
 
     /// <summary>
