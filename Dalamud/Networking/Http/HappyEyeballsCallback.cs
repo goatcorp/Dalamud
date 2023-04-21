58d91111410715b572ccf1ca0e4882a826556721
Kaz Wolfe <root@kazwolfe.io>
2023-04-20 23:11:38 -0700

    WIP: Happy Eyeballs HTTPClient
    - Create HappyEyeballsCallback to provide and track state around a callback for SocketsHttpHandler.
    - Create HappyHttpClient to provide easy access to a shared HTTP client service

---
+++ Dalamud/Networking/Http/HappyEyeballsCallback.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dalamud.Networking.Http;
+
+// Inspired by and adapted from https://github.com/jellyfin/jellyfin/pull/8598
+
+/// <summary>
+/// A class to provide a <see cref="SocketsHttpHandler.ConnectCallback"/> method (and tracked state) to implement a
+/// variant of the Happy Eyeballs algorithm for HTTP connections to dual-stack servers.
+///
+/// Each instance of this class tracks its own state.
+/// </summary>
+public class HappyEyeballsCallback
+{
+    private readonly Dictionary<DnsEndPoint, AddressFamily> addressFamilyCache = new();
+
+    private readonly AddressFamily? forcedAddressFamily;
+    private readonly int ipv4WaitMillis;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HappyHttpSocketsCallback"/> class.
+    /// </summary>
+    /// <param name="forcedAddressFamily">Optional override to force a specific AddressFamily.</param>
+    /// <param name="ipv4WaitMillis">Time to wait before initiating the IPv4 request.</param>
+    public HappyEyeballsCallback(AddressFamily? forcedAddressFamily = null, int ipv4WaitMillis = 100)
+    {
+        this.forcedAddressFamily = forcedAddressFamily;
+        this.ipv4WaitMillis = ipv4WaitMillis;
+    }
+
+    /// <summary>
+    /// The connection callback to provide to a <see cref="SocketsHttpHandler"/>.
+    /// </summary>
+    /// <param name="context">The context for an HTTP connection.</param>
+    /// <param name="token">The cancellation token to abort this request.</param>
+    /// <returns>Returns a Stream for consumption by HttpClient.</returns>
+    public async ValueTask<Stream> ConnectCallback(SocketsHttpConnectionContext context, CancellationToken token)
+    {
+        var addressFamilyOverride = this.GetAddressFamilyOverride(context);
+
+        if (addressFamilyOverride.HasValue)
+        {
+            return this.AttemptConnection(addressFamilyOverride.Value, context, token).GetAwaiter().GetResult();
+        }
+
+        using var linkedToken = CancellationTokenSource.CreateLinkedTokenSource(token);
+        var tryV6 = this.AttemptConnection(AddressFamily.InterNetworkV6, context, linkedToken.Token);
+        var tryV4 = this.AttemptConnection(AddressFamily.InterNetwork, context, linkedToken.Token, this.ipv4WaitMillis);
+
+        var victor = await Task.WhenAny(tryV6, tryV4).ConfigureAwait(false);
+        if (victor.IsCompletedSuccessfully)
+        {
+            var victorStream = victor.GetAwaiter().GetResult();
+
+            this.addressFamilyCache[context.DnsEndPoint] = victorStream.Socket.AddressFamily;
+
+            return victorStream;
+        }
+
+        // The loser can still fail, but we'll wait for it.
+        // If it succeeds, cache the result. If not, just throw the exception up the chain.
+        var loser = victor == tryV6 ? tryV4 : tryV6;
+        var loserStream = loser.GetAwaiter().GetResult();
+
+        this.addressFamilyCache[context.DnsEndPoint] = loserStream.Socket.AddressFamily;
+
+        return loserStream;
+    }
+
+    private AddressFamily? GetAddressFamilyOverride(SocketsHttpConnectionContext context)
+    {
+        if (this.forcedAddressFamily.HasValue)
+        {
+            return this.forcedAddressFamily.Value;
+        }
+
+        if (this.addressFamilyCache.TryGetValue(context.DnsEndPoint, out var cachedValue))
+        {
+            // TODO: Find some way to delete this after a while. It shouldn't stick around _forever_.
+            return cachedValue;
+        }
+
+        return null;
+    }
+
+    private async Task<NetworkStream> AttemptConnection(
+        AddressFamily family, SocketsHttpConnectionContext context, CancellationToken token, int delayMillis = 0)
+    {
+        if (delayMillis > 0)
+        {
+            await Task.Delay(delayMillis, token);
+            token.ThrowIfCancellationRequested();
+        }
+
+        var socket = new Socket(family, SocketType.Stream, ProtocolType.Tcp)
+        {
+            NoDelay = true,
+        };
+
+        try
+        {
+            await socket.ConnectAsync(context.DnsEndPoint, token).ConfigureAwait(false);
+            return new NetworkStream(socket, ownsSocket: true);
+        }
+        catch
+        {
+            socket.Dispose();
+            throw;
+        }
+    }
+}
---
+++ Dalamud/Networking/Http/HappyHttpClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Dalamud.IoC;
+
+namespace Dalamud.Networking.Http;
+
+/// <summary>
+/// A service to help build and manage HttpClients with some semblance of Happy Eyeballs (RFC 8305 - IPv4 fallback)
+/// awareness.
+/// </summary>
+[PluginInterface]
+[ServiceManager.BlockingEarlyLoadedService]
+public class HappyHttpClient : IDisposable, IServiceType
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HappyHttpClient"/> class.
+    ///
+    /// A service to talk to the Smileton Loporrits to build an HTTP Client aware of Happy Eyeballs.
+    /// </summary>
+    [ServiceManager.ServiceConstructor]
+    private HappyHttpClient()
+    {
+        this.SharedSocketsHandler = new SocketsHttpHandler
+        {
+            AutomaticDecompression = DecompressionMethods.All,
+            ConnectCallback = new HappyEyeballsCallback().ConnectCallback,
+        };
+
+        this.SharedHttpClient = new HttpClient(this.SharedSocketsHandler, false);
+    }
+
+    /// <summary>
+    /// Gets a <see cref="HttpClient"/> meant to be shared across all (standard) requests made by the application,
+    /// where custom configurations are not required.
+    ///
+    /// May or may not have been properly tested by the Loporrits.
+    /// </summary>
+    public HttpClient SharedHttpClient { get; }
+
+    /// <summary>
+    /// Gets a <see cref="SocketsHttpHandler"/> meant to be shared across any custom <see cref="HttpClient"/>s that
+    /// need to be made in other parts of the application.
+    ///
+    /// This handler comes pre-loaded with a common <see cref="HappyEyeballsCallback"/> for your convenience.
+    /// </summary>
+    public SocketsHttpHandler SharedSocketsHandler { get; }
+
+    /// <inheritdoc/>
+    void IDisposable.Dispose()
+    {
+        this.SharedHttpClient.Dispose();
+        this.SharedSocketsHandler.Dispose();
+
+        GC.SuppressFinalize(this);
+    }
+}
