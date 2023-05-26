7bbc08b7f7be700291efc41e8fedf281c3f50239
Ava Chaney <hello@ava.dev>
2023-05-25 19:59:01 -0700

    remove support for legacy devPlugins directory

--- Dalamud.Injector/EntryPoint.cs
+++ Dalamud.Injector/EntryPoint.cs
@@ -247,7 +247,6 @@ namespace Dalamud.Injector
             var workingDirectory = startInfo.WorkingDirectory;
             var configurationPath = startInfo.ConfigurationPath;
             var pluginDirectory = startInfo.PluginDirectory;
-            var defaultPluginDirectory = startInfo.DefaultPluginDirectory;
             var assetDirectory = startInfo.AssetDirectory;
             var delayInitializeMs = startInfo.DelayInitializeMs;
             var logName = startInfo.LogName;
@@ -262,8 +261,6 @@ namespace Dalamud.Injector
                     configurationPath = args[i][key.Length..];
                 else if (args[i].StartsWith(key = "--dalamud-plugin-directory="))
                     pluginDirectory = args[i][key.Length..];
-                else if (args[i].StartsWith(key = "--dalamud-dev-plugin-directory="))
-                    defaultPluginDirectory = args[i][key.Length..];
                 else if (args[i].StartsWith(key = "--dalamud-asset-directory="))
                     assetDirectory = args[i][key.Length..];
                 else if (args[i].StartsWith(key = "--dalamud-delay-initialize="))
@@ -287,7 +284,6 @@ namespace Dalamud.Injector
             workingDirectory ??= Directory.GetCurrentDirectory();
             configurationPath ??= Path.Combine(xivlauncherDir, "dalamudConfig.json");
             pluginDirectory ??= Path.Combine(xivlauncherDir, "installedPlugins");
-            defaultPluginDirectory ??= Path.Combine(xivlauncherDir, "devPlugins");
             assetDirectory ??= Path.Combine(xivlauncherDir, "dalamudAssets", "dev");
 
             ClientLanguage clientLanguage;
@@ -313,7 +309,6 @@ namespace Dalamud.Injector
             startInfo.WorkingDirectory = workingDirectory;
             startInfo.ConfigurationPath = configurationPath;
             startInfo.PluginDirectory = pluginDirectory;
-            startInfo.DefaultPluginDirectory = defaultPluginDirectory;
             startInfo.AssetDirectory = assetDirectory;
             startInfo.Language = clientLanguage;
             startInfo.DelayInitializeMs = delayInitializeMs;
