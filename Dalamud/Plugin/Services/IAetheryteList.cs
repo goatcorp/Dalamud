284001ce6b1c7f4a72fab6494ea8eb2cb5c40d77
goat <16760685+goaaats@users.noreply.github.com>
2023-06-13 20:10:47 +0200

    Interface support for plugin DI (#1235)
    * feat: interface support for plugin DI
    * attribute to indicate resolvability should be on the service instead of the interface

---
+++ Dalamud/Plugin/Services/IAetheryteList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+using Dalamud.Game.ClientState.Aetherytes;
+
+namespace Dalamud.Plugin.Services;
+
+/// <summary>
+/// This collection represents the list of available Aetherytes in the Teleport window.
+/// </summary>
+public interface IAetheryteList : IReadOnlyCollection<AetheryteEntry>
+{
+    /// <summary>
+    /// Gets the amount of Aetherytes the local player has unlocked.
+    /// </summary>
+    public int Length { get; }
+
+    /// <summary>
+    /// Gets a Aetheryte Entry at the specified index.
+    /// </summary>
+    /// <param name="index">Index.</param>
+    /// <returns>A <see cref="AetheryteEntry"/> at the specified index.</returns>
+    public AetheryteEntry? this[int index] { get; }
+}
