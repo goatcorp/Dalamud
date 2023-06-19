b901ad5aff9cf75c869403d49937312f1919f64a
goat <goatsdev@protonmail.com>
2023-06-19 19:19:04 +0200

    fix: move log retention out of EntryPoint.cs, redo retention behaviour for release

---
+++ Dalamud/Logging/Retention/DebugRetentionBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Dalamud.Logging.Retention;
+
+/// <summary>
+/// Class implementing log retention behaviour for debug builds.
+/// </summary>
+internal class DebugRetentionBehaviour : RetentionBehaviour
+{
+    /// <inheritdoc/>
+    public override void Apply(FileInfo logFile, FileInfo rolloverFile)
+    {
+        CullLogFile(logFile, 1 * 1024 * 1024, rolloverFile, 10 * 1024 * 1024);
+    }
+}
