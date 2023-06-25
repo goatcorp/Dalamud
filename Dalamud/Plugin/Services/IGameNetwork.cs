a3787498c9e8e48414c7af684bc2664018436040
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-24 23:38:28 -0700

    Add IGameNetwork (#1284)
    Co-authored-by: goat <16760685+goaaats@users.noreply.github.com>

---
+++ Dalamud/Plugin/Services/IGameNetwork.cs
@@ -0,0 +1,26 @@
+﻿using Dalamud.Game.Network;
+
+namespace Dalamud.Plugin.Services;
+
+/// <summary>
+/// This class handles interacting with game network events.
+/// </summary>
+public interface IGameNetwork
+{
+    // TODO(v9): we shouldn't be passing pointers to the actual data here
+
+    /// <summary>
+    /// The delegate type of a network message event.
+    /// </summary>
+    /// <param name="dataPtr">The pointer to the raw data.</param>
+    /// <param name="opCode">The operation ID code.</param>
+    /// <param name="sourceActorId">The source actor ID.</param>
+    /// <param name="targetActorId">The taret actor ID.</param>
+    /// <param name="direction">The direction of the packed.</param>
+    public delegate void OnNetworkMessageDelegate(nint dataPtr, ushort opCode, uint sourceActorId, uint targetActorId, NetworkMessageDirection direction);
+
+    /// <summary>
+    /// Event that is called when a network message is sent/received.
+    /// </summary>
+    public event OnNetworkMessageDelegate NetworkMessage;
+}
