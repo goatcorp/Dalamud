c7cab78873eed6013360da865d130f0d0f031b3c
Caraxi <git@caraxian.com>
2023-07-05 18:59:41 +0930

    Update ConfigChangeEvent.cs

--- Dalamud/Game/Config/ConfigChangeEvent.cs
+++ Dalamud/Game/Config/ConfigChangeEvent.cs
@@ -2,6 +2,6 @@
 
 namespace Dalamud.Game.Config;
 
-public abstract record ConfigChangeEvent(Enum GenericOption);
+public abstract record ConfigChangeEvent(Enum Option);
 
 public record ConfigChangeEvent<T>(T ConfigOption) : ConfigChangeEvent(ConfigOption) where T : Enum;
