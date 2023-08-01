5b6c90f122da0cf3b7bf3f20d103a2752fbf3e3b
Haselnussbomber <mail@haselnussbomber.de>
2023-08-01 21:55:51 +0200

    feat: default ItemKind param for AddItemLink

--- Dalamud/Game/Text/SeStringHandling/SeStringBuilder.cs
+++ Dalamud/Game/Text/SeStringHandling/SeStringBuilder.cs
@@ -123,7 +123,7 @@ public class SeStringBuilder
     /// <param name="kind">Kind of item to encode.</param>
     /// <param name="itemNameOverride">Override for the item's name.</param>
     /// <returns>The current builder.</returns>
-    public SeStringBuilder AddItemLink(uint itemId, ItemPayload.ItemKind kind, string? itemNameOverride = null) =>
+    public SeStringBuilder AddItemLink(uint itemId, ItemPayload.ItemKind kind = ItemPayload.ItemKind.Normal, string? itemNameOverride = null) =>
         this.Append(SeString.CreateItemLink(itemId, kind, itemNameOverride));
 
     /// <summary>
