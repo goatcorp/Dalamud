f342dae85545b51d3c3d7cb2393e83331a440233
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-08-27 13:11:04 -0700

    Add AddonLifecycle Service

---
+++ Dalamud/Game/AddonLifetime/AddonLifecycle.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using Dalamud.Hooking;
+using Dalamud.IoC;
+using Dalamud.IoC.Internal;
+using Dalamud.Logging;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace Dalamud.Game.AddonLifetime;
+
+/// <summary>
+/// This class provides events for in-game addon lifecycles.
+/// </summary>
+[PluginInterface]
+[InterfaceVersion("1.0")]
+[ServiceManager.EarlyLoadedService]
+#pragma warning disable SA1015
+[ResolveVia<IAddonLifecycle>]
+#pragma warning restore SA1015
+public unsafe class AddonLifecycle : IDisposable, IServiceType, IAddonLifecycle
+{
+    private readonly AddonLifecycleAddressResolver address;
+    private readonly Hook<AddonSetupDelegate> onAddonSetupHook;
+    private readonly Hook<AddonFinalizeDelegate> onAddonFinalizeHook;
+    
+    [ServiceManager.ServiceConstructor]
+    private AddonLifecycle(SigScanner sigScanner)
+    {
+        this.address = new AddonLifecycleAddressResolver();
+        this.address.Setup(sigScanner);
+
+        this.onAddonSetupHook = Hook<AddonSetupDelegate>.FromAddress(this.address.AddonSetup, this.OnAddonSetup);
+        this.onAddonFinalizeHook = Hook<AddonFinalizeDelegate>.FromAddress(this.address.AddonSetup, this.OnAddonFinalize);
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+    private delegate nint AddonSetupDelegate(AtkUnitBase* addon);
+
+    [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+    private delegate void AddonFinalizeDelegate(AtkUnitManager* unitManager, AtkUnitBase** atkUnitBase);
+    
+    /// <inheritdoc/>
+    public event Action<IAddonLifecycle.AddonArgs>? AddonPreSetup;
+    
+    /// <inheritdoc/>
+    public event Action<IAddonLifecycle.AddonArgs>? AddonPostSetup;
+    
+    /// <inheritdoc/>
+    public event Action<IAddonLifecycle.AddonArgs>? AddonPreFinalize;
+    
+    /// <inheritdoc/>
+    public event Action<IAddonLifecycle.AddonArgs>? AddonPostFinalize;
+    
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        this.onAddonSetupHook.Dispose();
+        this.onAddonFinalizeHook.Dispose();
+    }
+    
+    [ServiceManager.CallWhenServicesReady]
+    private void ContinueConstruction()
+    {
+        this.onAddonSetupHook.Enable();
+        this.onAddonFinalizeHook.Enable();
+    }
+
+    private nint OnAddonSetup(AtkUnitBase* addon)
+    {
+        try
+        {
+            this.AddonPreSetup?.Invoke(new IAddonLifecycle.AddonArgs { Addon = addon });
+        }
+        catch (Exception e)
+        {
+            PluginLog.Error(e, "[AddonLifecycle] Exception in OnAddonSetup pre-setup invoke.");
+        }
+
+        var result = this.onAddonSetupHook.Original(addon);
+
+        try
+        {
+            this.AddonPostSetup?.Invoke(new IAddonLifecycle.AddonArgs { Addon = addon });
+        }
+        catch (Exception e)
+        {
+            PluginLog.Error(e, "[AddonLifecycle] Exception in OnAddonSetup post-setup invoke.");
+        }
+
+        return result;
+    }
+
+    private void OnAddonFinalize(AtkUnitManager* unitManager, AtkUnitBase** atkUnitBase)
+    {
+        try
+        {
+            this.AddonPreFinalize?.Invoke(new IAddonLifecycle.AddonArgs { Addon = atkUnitBase[0] });
+        }
+        catch (Exception e)
+        {
+            PluginLog.Error(e, "[AddonLifecycle] Exception in OnAddonSetup pre-setup invoke.");
+        }
+
+        this.onAddonFinalizeHook.Original(unitManager, atkUnitBase);
+
+        try
+        {
+            this.AddonPostFinalize?.Invoke(new IAddonLifecycle.AddonArgs { Addon = atkUnitBase[0] });
+        }
+        catch (Exception e)
+        {
+            PluginLog.Error(e, "[AddonLifecycle] Exception in OnAddonSetup post-setup invoke.");
+        }
+    }
+}
---
+++ Dalamud/Plugin/Services/IAddonLifecycle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Dalamud.Memory;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace Dalamud.Plugin.Services;
+
+/// <summary>
+/// This class provides events for in-game addon lifecycles.
+/// </summary>
+public interface IAddonLifecycle
+{
+    /// <summary>
+    /// Event that fires before an addon is being setup.
+    /// </summary>
+    public event Action<AddonArgs> AddonPreSetup;
+    
+    /// <summary>
+    /// Event that fires after an addon is done being setup.
+    /// </summary>
+    public event Action<AddonArgs> AddonPostSetup;
+    
+    /// <summary>
+    /// Event that fires before an addon is being finalized.
+    /// </summary>
+    public event Action<AddonArgs> AddonPreFinalize;
+    
+    /// <summary>
+    /// Event that fires after an addon is done being finalized.
+    /// </summary>
+    public event Action<AddonArgs> AddonPostFinalize;
+    
+    /// <summary>
+    /// Addon argument data for use in event subscribers.
+    /// </summary>
+    public unsafe class AddonArgs
+    {
+        private string? addonName;
+        
+        /// <summary>
+        /// Gets the name of the addon this args referrers to.
+        /// </summary>
+        public string AddonName => this.addonName ??= MemoryHelper.ReadString(new nint(Addon->Name), 0x20).Split('\0')[0];
+
+        /// <summary>
+        /// Gets the pointer to the addons AtkUnitBase.
+        /// </summary>
+        required public AtkUnitBase* Addon { get; init; }
+    }
+}
