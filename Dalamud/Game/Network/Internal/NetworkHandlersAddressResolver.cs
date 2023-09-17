dce6574fbbfd6d031e5c536ceb58758fedc19169
Kaz Wolfe <root@kazwolfe.io>
2023-09-16 17:02:11 -0700

    Migrate CfPop off of opcodes

---
+++ Dalamud/Game/Network/Internal/NetworkHandlersAddressResolver.cs
@@ -0,0 +1,14 @@
+﻿namespace Dalamud.Game.Network.Internal;
+
+internal class NetworkHandlersAddressResolver : BaseAddressResolver
+{
+    /// <summary>
+    /// Gets or sets the pointer to the method responsible for handling CfPop packets.
+    /// </summary>
+    public nint CfPopPacketHandler { get; set; }
+
+    protected override void Setup64Bit(SigScanner scanner)
+    {
+        this.CfPopPacketHandler = scanner.ScanText("40 53 57 48 83 EC 78 48 8B D9 48 8D 0D");
+    }
+}
