43abb12710e1193f6e3ba898a932b3583826baba
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-09-21 21:55:56 -0700

    Add GameConfigPluginScoped (#1383)
    * Add GameConfigPluginScoped
    
    * Proposed Resolution to sub-object events
    
    * Nullify delegates to prevent memory leaks

--- Dalamud/Game/Config/GameConfig.cs
+++ Dalamud/Game/Config/GameConfig.cs
@@ -12,11 +12,7 @@ namespace Dalamud.Game.Config;
 /// This class represents the game's configuration.
 /// </summary>
 [InterfaceVersion("1.0")]
-[PluginInterface]
 [ServiceManager.EarlyLoadedService]
-#pragma warning disable SA1015
-[ResolveVia<IGameConfig>]
-#pragma warning restore SA1015
 internal sealed class GameConfig : IServiceType, IGameConfig, IDisposable
 {
     private readonly GameConfigAddressResolver address = new();
@@ -36,15 +32,30 @@ internal sealed class GameConfig : IServiceType, IGameConfig, IDisposable
         
             this.address.Setup(sigScanner);
             this.configChangeHook = Hook<ConfigChangeDelegate>.FromAddress(this.address.ConfigChangeAddress, this.OnConfigChanged);
-            this.configChangeHook?.Enable();
+            this.configChangeHook.Enable();
         });
     }
 
     private unsafe delegate nint ConfigChangeDelegate(ConfigBase* configBase, ConfigEntry* configEntry);
     
     /// <inheritdoc/>
-    public event EventHandler<ConfigChangeEvent> Changed;
+    public event EventHandler<ConfigChangeEvent>? Changed;
+
+    /// <summary>
+    /// Unused internally, used as a proxy for System.Changed via GameConfigPluginScoped
+    /// </summary>
+    public event EventHandler<ConfigChangeEvent>? SystemChanged;
     
+    /// <summary>
+    /// Unused internally, used as a proxy for UiConfig.Changed via GameConfigPluginScoped
+    /// </summary>
+    public event EventHandler<ConfigChangeEvent>? UiConfigChanged;
+    
+    /// <summary>
+    /// Unused internally, used as a proxy for UiControl.Changed via GameConfigPluginScoped
+    /// </summary>
+    public event EventHandler<ConfigChangeEvent>? UiControlChanged;
+
     /// <inheritdoc/>
     public GameConfigSection System { get; private set; }
 
@@ -191,4 +202,205 @@ internal sealed class GameConfig : IServiceType, IGameConfig, IDisposable
         
         return returnValue;
     }
+}
+
+/// <summary>
+/// Plugin-scoped version of a GameConfig service.
+/// </summary>
+[PluginInterface]
+[InterfaceVersion("1.0")]
+[ServiceManager.ScopedService]
+#pragma warning disable SA1015
+[ResolveVia<IGameConfig>]
+#pragma warning restore SA1015
+internal class GameConfigPluginScoped : IDisposable, IServiceType, IGameConfig
+{
+    [ServiceManager.ServiceDependency]
+    private readonly GameConfig gameConfigService = Service<GameConfig>.Get();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameConfigPluginScoped"/> class.
+    /// </summary>
+    internal GameConfigPluginScoped()
+    {
+        this.gameConfigService.Changed += this.ConfigChangedForward;
+        this.gameConfigService.System.Changed += this.SystemConfigChangedForward;
+        this.gameConfigService.UiConfig.Changed += this.UiConfigConfigChangedForward;
+        this.gameConfigService.UiControl.Changed += this.UiControlConfigChangedForward;
+    }
+    
+    /// <inheritdoc/>
+    public event EventHandler<ConfigChangeEvent>? Changed;
+    
+    /// <inheritdoc/>
+    public event EventHandler<ConfigChangeEvent>? SystemChanged;
+    
+    /// <inheritdoc/>
+    public event EventHandler<ConfigChangeEvent>? UiConfigChanged;
+    
+    /// <inheritdoc/>
+    public event EventHandler<ConfigChangeEvent>? UiControlChanged;
+    
+    /// <inheritdoc/>
+    public GameConfigSection System => this.gameConfigService.System;
+
+    /// <inheritdoc/>
+    public GameConfigSection UiConfig => this.gameConfigService.UiConfig;
+
+    /// <inheritdoc/>
+    public GameConfigSection UiControl => this.gameConfigService.UiControl;
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        this.gameConfigService.Changed -= this.ConfigChangedForward;
+        this.gameConfigService.System.Changed -= this.SystemConfigChangedForward;
+        this.gameConfigService.UiConfig.Changed -= this.UiConfigConfigChangedForward;
+        this.gameConfigService.UiControl.Changed -= this.UiControlConfigChangedForward;
+
+        this.Changed = null;
+        this.SystemChanged = null;
+        this.UiConfigChanged = null;
+        this.UiControlChanged = null;
+    }
+
+    /// <inheritdoc/>
+    public bool TryGet(SystemConfigOption option, out bool value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(SystemConfigOption option, out uint value)
+        => this.gameConfigService.TryGet(option, out value);
+
+    /// <inheritdoc/>
+    public bool TryGet(SystemConfigOption option, out float value)
+        => this.gameConfigService.TryGet(option, out value);
+
+    /// <inheritdoc/>
+    public bool TryGet(SystemConfigOption option, out string value)
+        => this.gameConfigService.TryGet(option, out value);
+
+    /// <inheritdoc/>
+    public bool TryGet(SystemConfigOption option, out UIntConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+
+    /// <inheritdoc/>
+    public bool TryGet(SystemConfigOption option, out FloatConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+    
+    /// <inheritdoc/>
+    public bool TryGet(SystemConfigOption option, out StringConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiConfigOption option, out bool value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiConfigOption option, out uint value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiConfigOption option, out float value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiConfigOption option, out string value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiConfigOption option, out UIntConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiConfigOption option, out FloatConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiConfigOption option, out StringConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiControlOption option, out bool value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiControlOption option, out uint value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiControlOption option, out float value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiControlOption option, out string value)
+        => this.gameConfigService.TryGet(option, out value);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiControlOption option, out UIntConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiControlOption option, out FloatConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+    
+    /// <inheritdoc/>
+    public bool TryGet(UiControlOption option, out StringConfigProperties? properties)
+        => this.gameConfigService.TryGet(option, out properties);
+    
+    /// <inheritdoc/>
+    public void Set(SystemConfigOption option, bool value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(SystemConfigOption option, uint value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(SystemConfigOption option, float value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(SystemConfigOption option, string value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(UiConfigOption option, bool value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(UiConfigOption option, uint value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(UiConfigOption option, float value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(UiConfigOption option, string value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(UiControlOption option, bool value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(UiControlOption option, uint value)
+        => this.gameConfigService.Set(option, value);
+    
+    /// <inheritdoc/>
+    public void Set(UiControlOption option, float value)
+        => this.gameConfigService.Set(option, value);
+
+    /// <inheritdoc/>
+    public void Set(UiControlOption option, string value)
+        => this.gameConfigService.Set(option, value);
+    
+    private void ConfigChangedForward(object sender, ConfigChangeEvent data) => this.Changed?.Invoke(sender, data);
+    
+    private void SystemConfigChangedForward(object sender, ConfigChangeEvent data) => this.SystemChanged?.Invoke(sender, data);
+    
+    private void UiConfigConfigChangedForward(object sender, ConfigChangeEvent data) => this.UiConfigChanged?.Invoke(sender, data);
+    
+    private void UiControlConfigChangedForward(object sender, ConfigChangeEvent data) => this.UiControlChanged?.Invoke(sender, data);
 }
