75460ffd568f5a3b7acd3beff144764230d21574
Kaz Wolfe <root@kazwolfe.io>
2023-09-23 16:27:11 -0700

    Inheritance is *still* something you can do!
    - Doc updates

--- Dalamud/Game/SigScanner.cs
+++ Dalamud/Game/SigScanner.cs
@@ -14,102 +14,15 @@ namespace Dalamud.Game;
 #pragma warning disable SA1015
 [ResolveVia<ISigScanner>]
 #pragma warning restore SA1015
-internal class SigScanner : IDisposable, IServiceType, ISigScanner
+internal class SigScanner : GenericSigScanner, IServiceType
 {
-    private GenericSigScanner scanner;
-    
     /// <summary>
     /// Initializes a new instance of the <see cref="SigScanner"/> class using the main module of the current process.
     /// </summary>
     /// <param name="doCopy">Whether or not to copy the module upon initialization for search operations to use, as to not get disturbed by possible hooks.</param>
     /// <param name="cacheFile">File used to cached signatures.</param>
     internal SigScanner(bool doCopy = false, FileInfo? cacheFile = null)
+        : base(Process.GetCurrentProcess().MainModule!, doCopy, cacheFile)
     {
-        this.scanner = new GenericSigScanner(Process.GetCurrentProcess().MainModule!, doCopy, cacheFile);
     }
-
-    /// <inheritdoc/>
-    public bool IsCopy => this.scanner.IsCopy;
-    
-    /// <inheritdoc/>
-    public bool Is32BitProcess => this.scanner.Is32BitProcess;
-    
-    /// <inheritdoc/>
-    public IntPtr SearchBase => this.scanner.SearchBase;
-    
-    /// <inheritdoc/>
-    public IntPtr TextSectionBase => this.scanner.TextSectionBase;
-    
-    /// <inheritdoc/>
-    public long TextSectionOffset => this.scanner.TextSectionOffset;
-    
-    /// <inheritdoc/>
-    public int TextSectionSize => this.scanner.TextSectionSize;
-    
-    /// <inheritdoc/>
-    public IntPtr DataSectionBase => this.scanner.DataSectionBase;
-    
-    /// <inheritdoc/>
-    public long DataSectionOffset => this.scanner.DataSectionOffset;
-    
-    /// <inheritdoc/>
-    public int DataSectionSize => this.scanner.DataSectionSize;
-    
-    /// <inheritdoc/>
-    public IntPtr RDataSectionBase => this.scanner.RDataSectionBase;
-    
-    /// <inheritdoc/>
-    public long RDataSectionOffset => this.scanner.RDataSectionOffset;
-    
-    /// <inheritdoc/>
-    public int RDataSectionSize => this.scanner.RDataSectionSize;
-    
-    /// <inheritdoc/>
-    public ProcessModule Module => this.scanner.Module;
-    
-    /// <inheritdoc/>
-    public void Dispose()
-    {
-        this.scanner.Dispose();
-    }
-
-    /// <inheritdoc/>
-    public IntPtr GetStaticAddressFromSig(string signature, int offset = 0) =>
-        this.scanner.GetStaticAddressFromSig(signature, offset);
-
-    /// <inheritdoc/>
-    public bool TryGetStaticAddressFromSig(string signature, out IntPtr result, int offset = 0) =>
-        this.scanner.TryGetStaticAddressFromSig(signature, out result, offset);
-
-    /// <inheritdoc/>
-    public IntPtr ScanData(string signature) =>
-        this.scanner.ScanData(signature);
-
-    /// <inheritdoc/>
-    public bool TryScanData(string signature, out IntPtr result) =>
-        this.scanner.TryScanData(signature, out result);
-
-    /// <inheritdoc/>
-    public IntPtr ScanModule(string signature) =>
-        this.scanner.ScanModule(signature);
-
-    /// <inheritdoc/>
-    public bool TryScanModule(string signature, out IntPtr result) =>
-        this.scanner.TryScanModule(signature, out result);
-
-    /// <inheritdoc/>
-    public IntPtr ResolveRelativeAddress(IntPtr nextInstAddr, int relOffset) =>
-        this.scanner.ResolveRelativeAddress(nextInstAddr, relOffset);
-
-    /// <inheritdoc/>
-    public IntPtr ScanText(string signature) =>
-        this.scanner.ScanText(signature);
-
-    /// <inheritdoc/>
-    public bool TryScanText(string signature, out IntPtr result) =>
-        this.scanner.TryScanText(signature, out result);
-
-    /// <inheritdoc cref="GenericSigScanner.Save()"/>
-    internal void Save() =>
-        this.scanner.Save();
 }
