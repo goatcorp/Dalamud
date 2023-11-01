dfe7a6370486669da716d7ddbe791e85ff569930
Kaz Wolfe <root@kazwolfe.io>
2023-10-31 19:57:09 -0700

    chore: Bump CS to 9f22a2a2
    - Revert of AtkResNode DrawFlags

--- Dalamud/Interface/Internal/UiDebug.cs
+++ Dalamud/Interface/Internal/UiDebug.cs
@@ -215,7 +215,7 @@ internal unsafe class UiDebug
                         while (b > byte.MaxValue) b -= byte.MaxValue;
                         while (b < byte.MinValue) b += byte.MaxValue;
                         textNode->AlignmentFontType = (byte)b;
-                        textNode->AtkResNode.ViewFlags |= NodeViewFlags.IsDirty;
+                        textNode->AtkResNode.DrawFlags |= 0x1;
                     }
 
                     ImGui.Text($"Color: #{textNode->TextColor.R:X2}{textNode->TextColor.G:X2}{textNode->TextColor.B:X2}{textNode->TextColor.A:X2}");
