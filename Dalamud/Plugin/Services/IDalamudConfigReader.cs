ec3974e47860361c2fce922b2be664d15b23b3ea
Kaz Wolfe <root@kazwolfe.io>
2023-11-25 12:09:59 -0800

    feat: Add `IDalamudConfigReader` service
    New service designed to expose certain Dalamud configs to plugins as read-only.

---
+++ Dalamud/Plugin/Services/IDalamudConfigReader.cs
@@ -0,0 +1,28 @@
+﻿using Dalamud.Configuration.Internal;
+using Dalamud.Configuration.Internal.Types;
+
+namespace Dalamud.Plugin.Services;
+
+/// <summary>
+/// A service for giving plugins access to select Dalamud settings.
+/// </summary>
+public interface IDalamudConfigReader
+{
+    /// <inheritdoc cref="DalamudConfiguration.IsMbCollect"/>
+    public bool IsMbCollect { get; }
+    
+    /// <inheritdoc cref="DalamudConfiguration.DisableRmtFiltering"/>
+    public bool DisableRmtFiltering { get; }
+    
+    /// <inheritdoc cref="DalamudConfiguration.IsAntiAntiDebugEnabled"/>
+    public bool IsAntiAntiDebugEnabled { get; }
+    
+    /// <inheritdoc cref="DalamudConfiguration.EnablePluginUISoundEffects"/>
+    public bool EnablePluginUISoundEffects { get; }
+    
+    /// <inheritdoc cref="DalamudConfiguration.AutoUpdatePlugins"/>
+    public bool AutoUpdatePlugins { get; }
+    
+    /// <inheritdoc cref="DalamudConfiguration.DoButtonsSystemMenu"/>
+    public bool DoButtonsSystemMenu { get; }
+}
