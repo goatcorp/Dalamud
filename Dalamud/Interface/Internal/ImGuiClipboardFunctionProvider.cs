5fbc501d8c0ce6727922ae17bc6b7d4323a7be28
Soreepeong <soreepeong@gmail.com>
2023-12-21 23:28:48 +0900

    Fix missing GlobalUnlock

--- Dalamud/Interface/Internal/ImGuiClipboardFunctionProvider.cs
+++ Dalamud/Interface/Internal/ImGuiClipboardFunctionProvider.cs
@@ -159,9 +159,9 @@ internal sealed unsafe class ImGuiClipboardFunctionProvider : IServiceType, IDis
             return this.clipboardData.Data;
         }
 
+        var hMem = (HGLOBAL)GetClipboardData(CF.CF_UNICODETEXT);
         try
         {
-            var hMem = (HGLOBAL)GetClipboardData(CF.CF_UNICODETEXT);
             if (hMem != default)
             {
                 var ptr = (char*)GlobalLock(hMem);
@@ -192,6 +192,8 @@ internal sealed unsafe class ImGuiClipboardFunctionProvider : IServiceType, IDis
         }
         finally
         {
+            if (hMem != default)
+                GlobalUnlock(hMem);
             CloseClipboard();
         }
 
