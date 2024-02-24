199722d29a4d85cb8020cac62fa6fbb2280c9329
Soreepeong <soreepeong@gmail.com>
2024-02-25 06:22:54 +0900

    Set IconTask on ActiveNotification ctor

--- Dalamud/Interface/Internal/Notifications/ActiveNotification.cs
+++ Dalamud/Interface/Internal/Notifications/ActiveNotification.cs
@@ -43,6 +43,7 @@ internal sealed class ActiveNotification : IActiveNotification, IDisposable
         this.hideEasing = new OutCubic(NotificationConstants.HideAnimationDuration);
 
         this.showEasing.Start();
+        this.UpdateIcon();
     }
 
     /// <inheritdoc/>
