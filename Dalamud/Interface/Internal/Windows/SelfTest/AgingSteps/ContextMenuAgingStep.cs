8e2f60389040d8ea2e39d648a9188acb3b5bf3e1
Asriel Camora <asriel@camora.dev>
2024-02-25 16:37:38 -0800

    Reimplement context menu self-test

--- Dalamud/Interface/Internal/Windows/SelfTest/AgingSteps/ContextMenuAgingStep.cs
+++ Dalamud/Interface/Internal/Windows/SelfTest/AgingSteps/ContextMenuAgingStep.cs
@@ -1,10 +1,14 @@
-﻿/*using System;
-
 using Dalamud.Data;
+using Dalamud.Game.Gui.ContextMenu;
+using Dalamud.Game.Network.Structures.InfoProxy;
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Utility;
+
 using ImGuiNET;
+
+using Lumina.Excel;
 using Lumina.Excel.GeneratedSheets;
-using Serilog;*/
 
 namespace Dalamud.Interface.Internal.Windows.SelfTest.AgingSteps;
 
@@ -13,31 +17,20 @@ namespace Dalamud.Interface.Internal.Windows.SelfTest.AgingSteps;
 /// </summary>
 internal class ContextMenuAgingStep : IAgingStep
 {
-    /*
     private SubStep currentSubStep;
 
-    private uint clickedItemId;
-    private bool clickedItemHq;
-    private uint clickedItemCount;
+    private bool? targetInventorySubmenuOpened;
+    private CharacterData? targetCharacter;
 
-    private string? clickedPlayerName;
-    private ushort? clickedPlayerWorld;
-    private ulong? clickedPlayerCid;
-    private uint? clickedPlayerId;
-
-    private bool multipleTriggerOne;
-    private bool multipleTriggerTwo;
+    private ExcelSheet<Item> itemSheet;
 
     private enum SubStep
     {
         Start,
-        TestItem,
-        TestGameObject,
-        TestSubMenu,
-        TestMultiple,
+        TestInventoryAndSubmenu,
+        TestDefault,
         Finish,
     }
-    */
 
     /// <inheritdoc/>
     public string Name => "Test Context Menu";
@@ -45,23 +38,22 @@ internal class ContextMenuAgingStep : IAgingStep
     /// <inheritdoc/>
     public SelfTestStepResult RunStep()
     {
-        /*
         var contextMenu = Service<ContextMenu>.Get();
         var dataMgr = Service<DataManager>.Get();
+        this.itemSheet = dataMgr.GetExcelSheet<Item>()!;
 
         ImGui.Text(this.currentSubStep.ToString());
 
         switch (this.currentSubStep)
         {
             case SubStep.Start:
-                contextMenu.ContextMenuOpened += this.ContextMenuOnContextMenuOpened;
+                contextMenu.OnMenuOpened += this.OnMenuOpened;
                 this.currentSubStep++;
                 break;
-            case SubStep.TestItem:
-                if (this.clickedItemId != 0)
+            case SubStep.TestInventoryAndSubmenu:
+                if (this.targetInventorySubmenuOpened == true)
                 {
-                    var item = dataMgr.GetExcelSheet<Item>()!.GetRow(this.clickedItemId);
-                    ImGui.Text($"Did you click \"{item!.Name.RawString}\", hq:{this.clickedItemHq}, count:{this.clickedItemCount}?");
+                    ImGui.Text($"Is the data in the submenu correct?");
 
                     if (ImGui.Button("Yes"))
                         this.currentSubStep++;
@@ -81,10 +73,10 @@ internal class ContextMenuAgingStep : IAgingStep
 
                 break;
 
-            case SubStep.TestGameObject:
-                if (!this.clickedPlayerName.IsNullOrEmpty())
+            case SubStep.TestDefault:
+                if (this.targetCharacter is { } character)
                 {
-                    ImGui.Text($"Did you click \"{this.clickedPlayerName}\", world:{this.clickedPlayerWorld}, cid:{this.clickedPlayerCid}, id:{this.clickedPlayerId}?");
+                    ImGui.Text($"Did you click \"{character.Name}\" ({character.ClassJob.GameData!.Abbreviation.ToDalamudString()})?");
 
                     if (ImGui.Button("Yes"))
                         this.currentSubStep++;
@@ -103,149 +95,83 @@ internal class ContextMenuAgingStep : IAgingStep
                 }
 
                 break;
-            case SubStep.TestSubMenu:
-                if (this.multipleTriggerOne && this.multipleTriggerTwo)
-                {
-                    this.currentSubStep++;
-                    this.multipleTriggerOne = this.multipleTriggerTwo = false;
-                }
-                else
-                {
-                    ImGui.Text("Right-click a character and select both options in the submenu.");
+            case SubStep.Finish:
+                return SelfTestStepResult.Pass;
 
-                    if (ImGui.Button("Skip"))
-                        this.currentSubStep++;
-                }
-
-                break;
-
-            case SubStep.TestMultiple:
-                if (this.multipleTriggerOne && this.multipleTriggerTwo)
-                {
-                    this.currentSubStep = SubStep.Finish;
-                    return SelfTestStepResult.Pass;
-                }
-
-                ImGui.Text("Select both options on any context menu.");
-                if (ImGui.Button("Skip"))
-                    this.currentSubStep++;
-                break;
             default:
                 throw new ArgumentOutOfRangeException();
         }
 
         return SelfTestStepResult.Waiting;
-        */
-
-        return SelfTestStepResult.Pass;
     }
-
+    
     /// <inheritdoc/>
     public void CleanUp()
     {
-        /*
         var contextMenu = Service<ContextMenu>.Get();
-        contextMenu.ContextMenuOpened -= this.ContextMenuOnContextMenuOpened;
+        contextMenu.OnMenuOpened -= this.OnMenuOpened;
 
         this.currentSubStep = SubStep.Start;
-        this.clickedItemId = 0;
-        this.clickedPlayerName = null;
-        this.multipleTriggerOne = this.multipleTriggerTwo = false;
-        */
+        this.targetInventorySubmenuOpened = null;
+        this.targetCharacter = null;
     }
 
-    /*
-    private void ContextMenuOnContextMenuOpened(ContextMenuOpenedArgs args)
+    private void OnMenuOpened(MenuOpenedArgs args)
     {
-        Log.Information("Got context menu with parent addon: {ParentAddonName}, title:{Title}, itemcnt:{ItemCount}", args.ParentAddonName, args.Title, args.Items.Count);
-        if (args.GameObjectContext != null)
-        {
-            Log.Information("   => GameObject:{GameObjectName} world:{World} cid:{Cid} id:{Id}", args.GameObjectContext.Name, args.GameObjectContext.WorldId, args.GameObjectContext.ContentId, args.GameObjectContext.Id);
-        }
-
-        if (args.InventoryItemContext != null)
-        {
-            Log.Information("   => Inventory:{ItemId} hq:{Hq} count:{Count}", args.InventoryItemContext.Id, args.InventoryItemContext.IsHighQuality, args.InventoryItemContext.Count);
-        }
-
         switch (this.currentSubStep)
         {
-            case SubStep.TestSubMenu:
-                args.AddCustomSubMenu("Aging Submenu", openedArgs =>
+            case SubStep.TestInventoryAndSubmenu:
+                if (args.MenuType == ContextMenuType.Inventory)
                 {
-                    openedArgs.AddCustomItem("Submenu Item 1", _ =>
+                    args.AddMenuItem(new()
                     {
-                        this.multipleTriggerOne = true;
-                    });
-
-                    openedArgs.AddCustomItem("Submenu Item 2", _ =>
-                    {
-                        this.multipleTriggerTwo = true;
-                    });
-                });
-
-                return;
-            case SubStep.TestMultiple:
-                args.AddCustomItem("Aging Item 1", _ =>
-                {
-                    this.multipleTriggerOne = true;
-                });
-
-                args.AddCustomItem("Aging Item 2", _ =>
-                {
-                    this.multipleTriggerTwo = true;
-                });
-
-                return;
-            case SubStep.Finish:
-                return;
-
-            default:
-                switch (args.ParentAddonName)
-                {
-                    case "Inventory":
-                        if (this.currentSubStep != SubStep.TestItem)
-                            return;
-
-                        args.AddCustomItem("Aging Item", _ =>
+                        Name = "Aging Item",
+                        Priority = -1,
+                        IsSubmenu = true,
+                        OnClicked = (MenuItemClickedArgs a) =>
                         {
-                            this.clickedItemId = args.InventoryItemContext!.Id;
-                            this.clickedItemHq = args.InventoryItemContext!.IsHighQuality;
-                            this.clickedItemCount = args.InventoryItemContext!.Count;
-                            Log.Warning("Clicked item: {Id} hq:{Hq} count:{Count}", this.clickedItemId, this.clickedItemHq, this.clickedItemCount);
-                        });
-                        break;
+                            SeString name;
+                            uint count;
+                            var targetItem = (a.Target as MenuTargetInventory).TargetItem;
+                            if (targetItem is { } item)
+                            {
+                                name = (this.itemSheet.GetRow(item.ItemId)?.Name.ToDalamudString() ?? $"Unknown ({item.ItemId})") + (item.IsHq ? $" {SeIconChar.HighQuality.ToIconString()}" : string.Empty);
+                                count = item.Quantity;
+                            }
+                            else
+                            {
+                                name = "None";
+                                count = 0;
+                            }
 
-                    case null:
-                    case "_PartyList":
-                    case "ChatLog":
-                    case "ContactList":
-                    case "ContentMemberList":
-                    case "CrossWorldLinkshell":
-                    case "FreeCompany":
-                    case "FriendList":
-                    case "LookingForGroup":
-                    case "LinkShell":
-                    case "PartyMemberList":
-                    case "SocialList":
-                        if (this.currentSubStep != SubStep.TestGameObject || args.GameObjectContext == null || args.GameObjectContext.Name.IsNullOrEmpty())
-                            return;
+                            a.OpenSubmenu(new MenuItem[]
+                            {
+                            new()
+                            {
+                                Name = "Name: " + name,
+                                IsEnabled = false,
+                            },
+                            new()
+                            {
+                                Name = $"Count: {count}",
+                                IsEnabled = false,
+                            },
+                            });
 
-                        args.AddCustomItem("Aging Character", _ =>
-                        {
-                            this.clickedPlayerName = args.GameObjectContext.Name!;
-                            this.clickedPlayerWorld = args.GameObjectContext.WorldId;
-                            this.clickedPlayerCid = args.GameObjectContext.ContentId;
-                            this.clickedPlayerId = args.GameObjectContext.Id;
-
-                            Log.Warning("Clicked player: {Name} world:{World} cid:{Cid} id:{Id}", this.clickedPlayerName, this.clickedPlayerWorld, this.clickedPlayerCid, this.clickedPlayerId);
-                        });
-
-                        break;
+                            this.targetInventorySubmenuOpened = true;
+                        },
+                    });
                 }
 
                 break;
+
+            case SubStep.TestDefault:
+                if (args.Target is MenuTargetDefault { TargetCharacter: { } character })
+                    this.targetCharacter = character;
+                break;
+
+            case SubStep.Finish:
+                return;
         }
     }
-    */
 }
