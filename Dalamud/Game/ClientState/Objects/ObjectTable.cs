5df1e293c618452793dbc8e09127c31c9986015d
rootdarkarchon <root.darkarchon@outlook.com>
2024-03-13 16:54:44 +0100

    add cache to objecttable

--- Dalamud/Game/ClientState/Objects/ObjectTable.cs
+++ Dalamud/Game/ClientState/Objects/ObjectTable.cs
@@ -8,6 +8,7 @@ using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.IoC;
 using Dalamud.IoC.Internal;
 using Dalamud.Plugin.Services;
+
 using Serilog;
 
 namespace Dalamud.Game.ClientState.Objects;
@@ -25,18 +26,21 @@ internal sealed partial class ObjectTable : IServiceType, IObjectTable
 {
     private const int ObjectTableLength = 599;
 
-    private readonly ClientStateAddressResolver address;
+    private readonly ClientState clientState;
+    private GameObject?[] cachedObjectTable = new GameObject?[ObjectTableLength];
 
     [ServiceManager.ServiceConstructor]
-    private ObjectTable(ClientState clientState)
+    private ObjectTable(ClientState clientState, Framework framework)
     {
-        this.address = clientState.AddressResolver;
+        this.clientState = clientState;
 
-        Log.Verbose($"Object table address 0x{this.address.ObjectTable.ToInt64():X}");
+        framework.Update += this.FrameworkOnUpdate;
+
+        Log.Verbose($"Object table address 0x{this.clientState.AddressResolver.ObjectTable.ToInt64():X}");
     }
 
     /// <inheritdoc/>
-    public IntPtr Address => this.address.ObjectTable;
+    public IntPtr Address => this.clientState.AddressResolver.ObjectTable;
 
     /// <inheritdoc/>
     public int Length => ObjectTableLength;
@@ -46,8 +50,10 @@ internal sealed partial class ObjectTable : IServiceType, IObjectTable
     {
         get
         {
-            var address = this.GetObjectAddress(index);
-            return this.CreateObjectReference(address);
+            if (index >= ObjectTableLength || index < 0)
+                return null;
+
+            return this.cachedObjectTable[index];
         }
     }
 
@@ -75,15 +81,13 @@ internal sealed partial class ObjectTable : IServiceType, IObjectTable
         if (index < 0 || index >= ObjectTableLength)
             return IntPtr.Zero;
 
-        return *(IntPtr*)(this.address.ObjectTable + (8 * index));
+        return *(IntPtr*)(this.clientState.AddressResolver.ObjectTable + (8 * index));
     }
 
     /// <inheritdoc/>
     public unsafe GameObject? CreateObjectReference(IntPtr address)
     {
-        var clientState = Service<ClientState>.GetNullable();
-
-        if (clientState == null || clientState.LocalContentId == 0)
+        if (this.clientState.LocalContentId == 0)
             return null;
 
         if (address == IntPtr.Zero)
@@ -103,6 +107,14 @@ internal sealed partial class ObjectTable : IServiceType, IObjectTable
             ObjectKind.Ornament => new Npc(address),
             _ => new GameObject(address),
         };
+    }
+
+    private void FrameworkOnUpdate(IFramework framework)
+    {
+        for (var i = 0; i < ObjectTableLength; i++)
+        {
+            this.cachedObjectTable[i] = this.CreateObjectReference(this.GetObjectAddress(i));
+        }
     }
 }
 
