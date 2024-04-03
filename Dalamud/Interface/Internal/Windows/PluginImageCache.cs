532781308d6291a2c0117e0e73a8252358e2d91a
goat <goatsdev@protonmail.com>
2024-04-03 22:53:12 +0200

    remove april fools 24 code

--- Dalamud/Interface/Internal/Windows/PluginImageCache.cs
+++ Dalamud/Interface/Internal/Windows/PluginImageCache.cs
@@ -191,11 +191,6 @@ internal class PluginImageCache : IInternalDisposableService
     {
         iconTexture = null;
         loadedSince = null;
-        
-        // Wait for the horse icon list to be there, if applicable
-        var fools = Service<Fools24>.Get();
-        if (Fools24.IsDayApplicable() && fools.IsWaitingForIconList && !fools.Failed)
-            return false;
 
         if (manifest == null || manifest.InternalName == null)
         {
@@ -643,14 +638,6 @@ internal class PluginImageCache : IInternalDisposableService
     {
         if (isThirdParty)
             return manifest.IconUrl;
-        
-        var fools = Service<Fools24>.Get();
-        if (Fools24.IsDayApplicable())
-        {
-            var iconLink = fools.GetHorseIconLink(manifest.InternalName);
-            if (iconLink != null)
-                return iconLink;
-        }
 
         return MainRepoDip17ImageUrl.Format(manifest.Dip17Channel!, manifest.InternalName, "icon.png");
     }
