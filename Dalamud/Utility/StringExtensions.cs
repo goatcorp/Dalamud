dfe83e05bc59f5617a350e2537fcfba49dd5d782
kalilistic <35899782+kalilistic@users.noreply.github.com>
2024-06-14 21:42:03 -0400

    refactor: use client struct for character name check

--- Dalamud/Utility/StringExtensions.cs
+++ Dalamud/Utility/StringExtensions.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 
 namespace Dalamud.Utility;
 
@@ -31,36 +30,12 @@ public static class StringExtensions
     public static bool IsNullOrWhitespace([NotNullWhen(false)] this string? value) => string.IsNullOrWhiteSpace(value);
     
     /// <summary>
-    /// Validate if character name is valid.
-    /// Both forename and surname must be between 2 and 15 characters and not total more than 20 characters combined.
-    /// Only letters, hyphens, and apostrophes can be used.
-    /// The first character of either name must be a letter.
-    /// Hyphens cannot be used in succession or placed immediately before or after apostrophes.
+    /// Validate if character name is valid using game check.
     /// </summary>
     /// <param name="value">character name to validate.</param>
     /// <returns>indicator if character is name is valid.</returns>
     public static bool IsValidCharacterName(this string value)
     {
-        if (string.IsNullOrEmpty(value)) return false;
-        if (value.Length > 21) return false; // add 1 to allow for space
-        var names = value.Split(' ');
-        if (names.Length != 2) return false;
-        var forename = names[0];
-        var surname = names[1];
-        if (!IsValidName(forename)) return false;
-        if (!IsValidName(surname)) return false;
-        return true;
-    }
-
-    private static bool IsValidName(string name)
-    {
-        if (name.Length is < 2 or > 15) return false;
-        if (name.Any(c => !char.IsLetter(c) && !c.Equals('\'') && !c.Equals('-'))) return false;
-        if (!char.IsLetter(name[0])) return false;
-        if (!char.IsUpper(name[0])) return false;
-        if (name.Contains("--")) return false;
-        if (name.Contains("\'-")) return false;
-        if (name.Contains("-\'")) return false;
-        return true;
+        return !string.IsNullOrEmpty(value) && FFXIVClientStructs.FFXIV.Client.UI.UIModule.IsPlayerCharacterName(value);
     }
 }
