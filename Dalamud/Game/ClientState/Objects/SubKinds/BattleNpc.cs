6916ba0beab3b731ed455c626acb725299b37a5c
Kaz Wolfe <root@kazwolfe.io>
2024-06-28 16:21:02 -0700

    make IBattleNpc public

--- Dalamud/Game/ClientState/Objects/SubKinds/BattleNpc.cs
+++ Dalamud/Game/ClientState/Objects/SubKinds/BattleNpc.cs
@@ -29,7 +29,7 @@ internal unsafe class BattleNpc : BattleChara, IBattleNpc
 /// <summary>
 /// A interface that represents a battle NPC.
 /// </summary>
-internal interface IBattleNpc : IBattleChara
+public interface IBattleNpc : IBattleChara
 {
     /// <summary>
     /// Gets the BattleNpc <see cref="BattleNpcSubKind" /> of this BattleNpc.
