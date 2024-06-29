f9333e67a8ffe8b8bd86db5d13520902bb039f73
Pilzinsel64 <pilzinsel64@gmx.de>
2024-06-29 09:55:51 +0200

    rename Pointer to Address

--- Dalamud/Game/Gui/Nameplates/Model/NameplateElement.cs
+++ Dalamud/Game/Gui/Nameplates/Model/NameplateElement.cs
@@ -6,21 +6,21 @@ namespace Dalamud.Game.Gui.Nameplates.Model;
 /// <summary>
 /// Represents a nameplate text element.
 /// </summary>
-/// <param name="pointer">The pointer for the nameplate element.</param>
+/// <param name="address">The pointer for the nameplate element.</param>
 /// <param name="type">The type for the nameplate element.</param>
-internal class NameplateElement(nint pointer, NameplateElementType type) : INameplateElement
+internal class NameplateElement(nint address, NameplateElementType type) : INameplateElement
 {
     private SeString? text;
 
     /// <summary>
     /// Gets or sets the pointer for the text.
     /// </summary>
-    public nint Pointer { get; set; } = pointer;
+    public nint Address { get; set; } = address;
 
     /// <inheritdoc/>
     public SeString Text
     {
-        get => this.text ??= MemoryHelper.ReadSeStringNullTerminated(this.Pointer);
+        get => this.text ??= MemoryHelper.ReadSeStringNullTerminated(this.Address);
         set => this.text = value;
     }
 
