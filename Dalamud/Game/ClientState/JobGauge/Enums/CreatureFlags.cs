7176aca798245c1f078358be319029e8cae6f584
NostraThomas99 <34015422+NostraThomas99@users.noreply.github.com>
2024-07-03 18:15:51 -0500

    Add support for PCT and VPR to Dalamud.Game.ClientState.JobGauge
    Added PCTGauge and VPRGauge to the job gauge definitions. Also updated JobGauge widget to support VPR and PCT.

---
+++ Dalamud/Game/ClientState/JobGauge/Enums/CreatureFlags.cs
@@ -0,0 +1,32 @@
+﻿namespace Dalamud.Game.ClientState.JobGauge.Enums;
+
+/// <summary>
+/// Which creature flags are present on the gauge.
+/// </summary>
+public enum CreatureFlags : byte
+{
+    /// <summary>
+    /// Pom flag present
+    /// </summary>
+    Pom = 1,
+    
+    /// <summary>
+    /// Wings flag present
+    /// </summary>
+    Wings = 2,
+    
+    /// <summary>
+    /// Claw flag present
+    /// </summary>
+    Claw = 4,
+
+    /// <summary>
+    /// Moogle portrait present
+    /// </summary>
+    MooglePortait = 16,
+    
+    /// <summary>
+    /// Madeen portrait present
+    /// </summary>
+    MadeenPortrait = 32,
+}
---
+++ Dalamud/Game/ClientState/JobGauge/Types/VPRGauge.cs
@@ -0,0 +1,42 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Game.Gauge;
+
+using Reloaded.Memory;
+
+using DreadCombo = Dalamud.Game.ClientState.JobGauge.Enums.DreadCombo;
+
+namespace Dalamud.Game.ClientState.JobGauge.Types;
+
+/// <summary>
+/// In-memory VPR job gauge.
+/// </summary>
+public unsafe class VPRGauge : JobGaugeBase<ViperGauge>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VPRGauge"/> class.
+    /// </summary>
+    /// <param name="address">Address of the job gauge.</param>
+    internal VPRGauge(IntPtr address)
+        : base(address)
+    {
+    }
+    
+    /// <summary>
+    /// How many uses of uncoiled fury the player has.
+    /// </summary>
+    public byte RattlingCoilStacks => Struct->RattlingCoilStacks;
+
+    /// <summary>
+    /// Tracks AnguineTribute stacks and gauge.
+    /// </summary>
+    public byte SerpentOffering => Struct->SerpentOffering;
+    
+    /// <summary>
+    /// Allows the use of 1st, 2nd, 3rd, 4th generation and Ouroboros.
+    /// </summary>
+    public byte AnguineTribute => Struct->AnguineTribute;
+
+    /// <summary>
+    /// Keeps track of last Weaponskill used in DreadWinder/Pit of Dread combo.
+    /// </summary>
+    public DreadCombo DreadCombo => (DreadCombo)Struct->DreadCombo;
+}
