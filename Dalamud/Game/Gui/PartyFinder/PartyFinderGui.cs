1d8b3e38a9687d525c4da0564146d5c9802e3ad8
MidoriKami <mi_kami@outlook.com>
2024-07-12 20:37:46 -0700

    Add PartyFinderGui Nag

--- Dalamud/Game/Gui/PartyFinder/PartyFinderGui.cs
+++ Dalamud/Game/Gui/PartyFinder/PartyFinderGui.cs
@@ -5,6 +5,8 @@ using Dalamud.Game.Gui.PartyFinder.Types;
 using Dalamud.Hooking;
 using Dalamud.IoC;
 using Dalamud.IoC.Internal;
+using Dalamud.Logging.Internal;
+using Dalamud.Plugin.Internal.Types;
 using Dalamud.Plugin.Services;
 
 using Serilog;
@@ -132,14 +134,19 @@ internal sealed class PartyFinderGui : IInternalDisposableService, IPartyFinderG
 #pragma warning restore SA1015
 internal class PartyFinderGuiPluginScoped : IInternalDisposableService, IPartyFinderGui
 {
+    private static readonly ModuleLog Log = new("PartyFinderGui");
+    private readonly LocalPlugin plugin;
+
     [ServiceManager.ServiceDependency]
     private readonly PartyFinderGui partyFinderGuiService = Service<PartyFinderGui>.Get();
 
     /// <summary>
     /// Initializes a new instance of the <see cref="PartyFinderGuiPluginScoped"/> class.
     /// </summary>
-    internal PartyFinderGuiPluginScoped()
+    /// <param name="plugin">Information about the plugin using this service.</param>
+    internal PartyFinderGuiPluginScoped(LocalPlugin plugin)
     {
+        this.plugin = plugin;
         this.partyFinderGuiService.ReceiveListing += this.ReceiveListingForward;
     }
 
@@ -149,6 +156,11 @@ internal class PartyFinderGuiPluginScoped : IInternalDisposableService, IPartyFi
     /// <inheritdoc/>
     void IInternalDisposableService.DisposeService()
     {
+        if (this.ReceiveListing?.GetInvocationList().Length > 0)
+        {
+            Log.Warning($"{this.plugin.InternalName} is leaking {this.ReceiveListing?.GetInvocationList().Length} ReceiveListing listeners! Make sure that all of them are unregistered properly.");
+        }
+        
         this.partyFinderGuiService.ReceiveListing -= this.ReceiveListingForward;
 
         this.ReceiveListing = null;
