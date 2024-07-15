f816a32fd359b539e84672b64a810a1027f0bfe2
Ionite <dev@ionite.io>
2024-07-15 15:35:59 -0400

    Add UsedImplicitly to IDalamudPlugin

--- Dalamud/Plugin/IDalamudPlugin.cs
+++ Dalamud/Plugin/IDalamudPlugin.cs
@@ -1,8 +1,11 @@
+﻿using JetBrains.Annotations;
+
 namespace Dalamud.Plugin;
 
 /// <summary>
 /// This interface represents a basic Dalamud plugin. All plugins have to implement this interface.
 /// </summary>
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature,ImplicitUseTargetFlags.WithInheritors)]
 public interface IDalamudPlugin : IDisposable
 {
 }
