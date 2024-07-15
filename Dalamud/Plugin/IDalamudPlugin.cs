5668624488ead9154385ca65bf9551e9373e98dd
Ionite <dev@ionite.io>
2024-07-15 15:39:42 -0400

    Fix Formatting

--- Dalamud/Plugin/IDalamudPlugin.cs
+++ Dalamud/Plugin/IDalamudPlugin.cs
@@ -5,7 +5,7 @@ namespace Dalamud.Plugin;
 /// <summary>
 /// This interface represents a basic Dalamud plugin. All plugins have to implement this interface.
 /// </summary>
-[UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature,ImplicitUseTargetFlags.WithInheritors)]
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature, ImplicitUseTargetFlags.WithInheritors)]
 public interface IDalamudPlugin : IDisposable
 {
 }
