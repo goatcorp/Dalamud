ca9f36b38dfdcde1359d883a01000a56eee5be24
Kaz Wolfe <root@kazwolfe.io>
2024-08-05 14:51:04 -0700

    fix: XivChatType respects the configured chat channel if not set

--- Dalamud/Game/Text/XivChatEntry.cs
+++ Dalamud/Game/Text/XivChatEntry.cs
@@ -10,7 +10,7 @@ public sealed class XivChatEntry
     /// <summary>
     /// Gets or sets the type of entry.
     /// </summary>
-    public XivChatType Type { get; set; } = XivChatType.Debug;
+    public XivChatType? Type { get; set; }
 
     /// <summary>
     /// Gets or sets the message timestamp.
