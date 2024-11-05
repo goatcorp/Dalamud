367fa8eaade6f06fb59c6dd81e1cd6808e22f6fb
Haselnussbomber <mail@haselnussbomber.de>
2024-11-05 23:09:37 +0100

    Remove unused code from ChatHandlers

--- Dalamud/Game/ChatHandlers.cs
+++ Dalamud/Game/ChatHandlers.cs
@@ -1,22 +1,14 @@
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 
 using CheapLoc;
+
 using Dalamud.Configuration.Internal;
-using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Game.Gui;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
-using Dalamud.Game.Text.SeStringHandling.Payloads;
-using Dalamud.Interface;
-using Dalamud.Interface.ImGuiNotification;
-using Dalamud.Interface.ImGuiNotification.Internal;
 using Dalamud.Interface.Internal;
-using Dalamud.Interface.Internal.Windows;
 using Dalamud.Logging.Internal;
 using Dalamud.Plugin.Internal;
 using Dalamud.Utility;
@@ -27,49 +19,10 @@ namespace Dalamud.Game;
 /// Chat events and public helper functions.
 /// </summary>
 [ServiceManager.EarlyLoadedService]
-internal class ChatHandlers : IServiceType
+internal partial class ChatHandlers : IServiceType
 {
-    private static readonly ModuleLog Log = new("CHATHANDLER");
+    private static readonly ModuleLog Log = new("ChatHandlers");
 
-    private readonly Dictionary<ClientLanguage, Regex[]> retainerSaleRegexes = new()
-    {
-        {
-            ClientLanguage.Japanese,
-            new Regex[]
-            {
-                new Regex(@"^(?:.+)マーケットに(?<origValue>[\d,.]+)ギルで出品した(?<item>.*)×(?<count>[\d,.]+)が売れ、(?<value>[\d,.]+)ギルを入手しました。$", RegexOptions.Compiled),
-                new Regex(@"^(?:.+)マーケットに(?<origValue>[\d,.]+)ギルで出品した(?<item>.*)が売れ、(?<value>[\d,.]+)ギルを入手しました。$", RegexOptions.Compiled),
-            }
-        },
-        {
-            ClientLanguage.English,
-            new Regex[]
-            {
-                new Regex(@"^(?<item>.+) you put up for sale in the (?:.+) markets (?:have|has) sold for (?<value>[\d,.]+) gil \(after fees\)\.$", RegexOptions.Compiled),
-            }
-        },
-        {
-            ClientLanguage.German,
-            new Regex[]
-            {
-                new Regex(@"^Dein Gehilfe hat (?<item>.+) auf dem Markt von (?:.+) für (?<value>[\d,.]+) Gil verkauft\.$", RegexOptions.Compiled),
-                new Regex(@"^Dein Gehilfe hat (?<item>.+) auf dem Markt von (?:.+) verkauft und (?<value>[\d,.]+) Gil erhalten\.$", RegexOptions.Compiled),
-            }
-        },
-        {
-            ClientLanguage.French,
-            new Regex[]
-            {
-                new Regex(@"^Un servant a vendu (?<item>.+) pour (?<value>[\d,.]+) gil à (?:.+)\.$", RegexOptions.Compiled),
-            }
-        },
-    };
-
-    private readonly Regex urlRegex = new(@"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", RegexOptions.Compiled);
-
-    [ServiceManager.ServiceDependency]
-    private readonly Dalamud dalamud = Service<Dalamud>.Get();
-    
     [ServiceManager.ServiceDependency]
     private readonly DalamudConfiguration configuration = Service<DalamudConfiguration>.Get();
 
@@ -92,6 +45,9 @@ internal class ChatHandlers : IServiceType
     /// </summary>
     public bool IsAutoUpdateComplete { get; private set; }
 
+    [GeneratedRegex(@"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", RegexOptions.Compiled)]
+    private static partial Regex CompiledUrlRegex();
+
     private void OnCheckMessageHandled(XivChatType type, int timestamp, ref SeString sender, ref SeString message, ref bool isHandled)
     {
         var textVal = message.TextValue;
@@ -100,7 +56,7 @@ internal class ChatHandlers : IServiceType
             this.configuration.BadWords.Any(x => !string.IsNullOrEmpty(x) && textVal.Contains(x)))
         {
             // This seems to be in the user block list - let's not show it
-            Log.Debug("Blocklist triggered");
+            Log.Debug("Filtered a message that contained a muted word");
             isHandled = true;
             return;
         }
@@ -127,41 +83,10 @@ internal class ChatHandlers : IServiceType
                 return;
 #endif
 
-        if (type == XivChatType.RetainerSale)
-        {
-            foreach (var regex in this.retainerSaleRegexes[(ClientLanguage)this.dalamud.StartInfo.Language])
-            {
-                var matchInfo = regex.Match(message.TextValue);
-
-                // we no longer really need to do/validate the item matching since we read the id from the byte array
-                // but we'd be checking the main match anyway
-                var itemInfo = matchInfo.Groups["item"];
-                if (!itemInfo.Success)
-                    continue;
-
-                var itemLink = message.Payloads.FirstOrDefault(x => x.Type == PayloadType.Item) as ItemPayload;
-                if (itemLink == default)
-                {
-                    Log.Error("itemLink was null. Msg: {0}", BitConverter.ToString(message.Encode()));
-                    break;
-                }
-
-                Log.Debug($"Probable retainer sale: {message}, decoded item {itemLink.Item.RowId}, HQ {itemLink.IsHQ}");
-
-                var valueInfo = matchInfo.Groups["value"];
-                // not sure if using a culture here would work correctly, so just strip symbols instead
-                if (!valueInfo.Success || !int.TryParse(valueInfo.Value.Replace(",", string.Empty).Replace(".", string.Empty), out var itemValue))
-                    continue;
-
-                // Task.Run(() => this.dalamud.BotManager.ProcessRetainerSale(itemLink.Item.RowId, itemValue, itemLink.IsHQ));
-                break;
-            }
-        }
-
         var messageCopy = message;
         var senderCopy = sender;
 
-        var linkMatch = this.urlRegex.Match(message.TextValue);
+        var linkMatch = CompiledUrlRegex().Match(message.TextValue);
         if (linkMatch.Value.Length > 0)
             this.LastLink = linkMatch.Value;
     }
