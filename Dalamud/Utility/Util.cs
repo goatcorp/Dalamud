6dd85c9e3ee3643b3b408a4661f6c3ddb215420e
goat <goatsdev@protonmail.com>
2024-12-29 16:56:31 +0100

    remove some unused stuff in Util, fix warnings

--- Dalamud/Utility/Util.cs
+++ Dalamud/Utility/Util.cs
@@ -65,7 +65,6 @@ public static class Util
     private static readonly Type GenericSpanType = typeof(Span<>);
     private static string? scmVersionInternal;
     private static string? gitHashInternal;
-    private static int? gitCommitCountInternal;
     private static string? gitHashClientStructsInternal;
 
     private static ulong moduleStartAddr;
@@ -76,58 +75,6 @@ public static class Util
     /// </summary>
     public static string AssemblyVersion { get; } =
         Assembly.GetAssembly(typeof(ChatHandlers)).GetName().Version.ToString();
-
-    /// <summary>
-    /// Check two byte arrays for equality.
-    /// </summary>
-    /// <param name="a1">The first byte array.</param>
-    /// <param name="a2">The second byte array.</param>
-    /// <returns>Whether or not the byte arrays are equal.</returns>
-    public static unsafe bool FastByteArrayCompare(byte[]? a1, byte[]? a2)
-    {
-        // Copyright (c) 2008-2013 Hafthor Stefansson
-        // Distributed under the MIT/X11 software license
-        // Ref: http://www.opensource.org/licenses/mit-license.php.
-        // https://stackoverflow.com/a/8808245
-
-        if (a1 == a2) return true;
-        if (a1 == null || a2 == null || a1.Length != a2.Length)
-            return false;
-        fixed (byte* p1 = a1, p2 = a2)
-        {
-            byte* x1 = p1, x2 = p2;
-            var l = a1.Length;
-            for (var i = 0; i < l / 8; i++, x1 += 8, x2 += 8)
-            {
-                if (*((long*)x1) != *((long*)x2))
-                    return false;
-            }
-
-            if ((l & 4) != 0)
-            {
-                if (*((int*)x1) != *((int*)x2))
-                    return false;
-                x1 += 4;
-                x2 += 4;
-            }
-
-            if ((l & 2) != 0)
-            {
-                if (*((short*)x1) != *((short*)x2))
-                    return false;
-                x1 += 2;
-                x2 += 2;
-            }
-
-            if ((l & 1) != 0)
-            {
-                if (*((byte*)x1) != *((byte*)x2))
-                    return false;
-            }
-
-            return true;
-        }
-    }
 
     /// <summary>
     /// Gets the SCM Version from the assembly, or null if it cannot be found. This method will generally return
