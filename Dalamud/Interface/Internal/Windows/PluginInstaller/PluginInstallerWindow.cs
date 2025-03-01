cd03ef7d02ac19aebaf5da1acbc7fdadb2122e64
nil <25884226+voidstar0@users.noreply.github.com>
2025-02-28 20:37:58 -0500

    fix: always scan for dev plugins when opening the plugin installer (#2184)
    * fix: scan for dev plugins on plugin installer window open
    
    * fix: show scan dev plugins button based on plugin locations and not installed plugins

--- Dalamud/Interface/Internal/Windows/PluginInstaller/PluginInstallerWindow.cs
+++ Dalamud/Interface/Internal/Windows/PluginInstaller/PluginInstallerWindow.cs
@@ -283,6 +283,7 @@ internal class PluginInstallerWindow : Window, IDisposable
         var pluginManager = Service<PluginManager>.Get();
 
         _ = pluginManager.ReloadPluginMastersAsync();
+        Service<PluginManager>.Get().ScanDevPlugins();
 
         if (!this.isSearchTextPrefilled) this.searchText = string.Empty;
         this.sortKind = PluginSortKind.Alphabetical;
@@ -755,8 +756,9 @@ internal class PluginInstallerWindow : Window, IDisposable
             Service<DalamudInterface>.Get().OpenSettings();
         }
 
-        // If any dev plugins are installed, allow a shortcut for the /xldev menu item
-        if (this.hasDevPlugins)
+        // If any dev plugin locations exist, allow a shortcut for the /xldev menu item
+        var hasDevPluginLocations = configuration.DevPluginLoadLocations.Count > 0;
+        if (hasDevPluginLocations)
         {
             ImGui.SameLine();
             if (ImGui.Button(Locs.FooterButton_ScanDevPlugins))
