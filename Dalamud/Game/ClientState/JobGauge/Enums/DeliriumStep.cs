23c87015b635ce7fa58a6b3608a8e9b99c3ca3c3
Ethan Henderson <ethan@zbee.codes>
2025-03-23 16:14:16 -0600

    add(DRKGauge): Add `DeliriumComboStep`, and its enum `DeliriumStep` (#2198)

---
+++ Dalamud/Game/ClientState/JobGauge/Enums/DeliriumStep.cs
@@ -0,0 +1,19 @@
+namespace Dalamud.Game.ClientState.JobGauge.Enums;
+
+public enum DeliriumStep
+{
+    /// <summary>
+    /// Scarlet Delirium can be used.
+    /// </summary>
+    SCARLET_DELIRIUM = 0,
+
+    /// <summary>
+    /// Comeuppance can be used.
+    /// </summary>
+    COMEUPPANCE = 1,
+
+    /// <summary>
+    /// Torcleaver can be used.
+    /// </summary>
+    TORCLEAVER = 2,
+}
