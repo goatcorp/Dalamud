cba1a7d18cf68edd6be62a08c7802f18da459edb
Kaz Wolfe <root@kazwolfe.io>
2025-03-25 16:17:21 -0700

    fix: AddonLifecycle sig, cs bump

--- Dalamud/Game/Addon/Lifecycle/AddonLifecycleAddressResolver.cs
+++ Dalamud/Game/Addon/Lifecycle/AddonLifecycleAddressResolver.cs
@@ -13,19 +13,19 @@ internal unsafe class AddonLifecycleAddressResolver : BaseAddressResolver
     /// This is called for a majority of all addon OnSetup's.
     /// </summary>
     public nint AddonSetup { get; private set; }
-    
+
     /// <summary>
     /// Gets the address of the other addon setup hook invoked by the AtkUnitManager.
     /// There are two callsites for this vFunc, we need to hook both of them to catch both normal UI and special UI cases like dialogue.
     /// This seems to be called rarely for specific addons.
     /// </summary>
     public nint AddonSetup2 { get; private set; }
-    
+
     /// <summary>
     /// Gets the address of the addon finalize hook invoked by the AtkUnitManager.
     /// </summary>
     public nint AddonFinalize { get; private set; }
-    
+
     /// <summary>
     /// Gets the address of the addon draw hook invoked by virtual function call.
     /// </summary>
@@ -35,7 +35,7 @@ internal unsafe class AddonLifecycleAddressResolver : BaseAddressResolver
     /// Gets the address of the addon update hook invoked by virtual function call.
     /// </summary>
     public nint AddonUpdate { get; private set; }
-    
+
     /// <summary>
     /// Gets the address of the addon onRequestedUpdate hook invoked by virtual function call.
     /// </summary>
@@ -51,6 +51,6 @@ internal unsafe class AddonLifecycleAddressResolver : BaseAddressResolver
         this.AddonFinalize = sig.ScanText("E8 ?? ?? ?? ?? 48 83 EF 01 75 D5");
         this.AddonDraw = sig.ScanText("FF 90 ?? ?? ?? ?? 83 EB 01 79 C4 48 81 EF ?? ?? ?? ?? 48 83 ED 01");
         this.AddonUpdate = sig.ScanText("FF 90 ?? ?? ?? ?? 40 88 AF ?? ?? ?? ?? 45 33 D2");
-        this.AddonOnRequestedUpdate = sig.ScanText("FF 90 98 01 00 00 48 8B 5C 24 30 48 83 C4 20");
+        this.AddonOnRequestedUpdate = sig.ScanText("FF 90 A0 01 00 00 48 8B 5C 24 30");
     }
 }
