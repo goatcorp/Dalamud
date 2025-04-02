ca438b6e69112fb0192df25530de177864229e11
goaaats <goatsdev@protonmail.com>
2025-04-03 00:32:37 +0200

    Add self-test for framework task scheduling

---
+++ Dalamud/Interface/Internal/Windows/SelfTest/Steps/FrameworkTaskSchedulerSelfTestStep.cs
@@ -0,0 +1,64 @@
+using System.Threading.Tasks;
+
+using Dalamud.Game;
+using Dalamud.Utility;
+
+namespace Dalamud.Interface.Internal.Windows.SelfTest.Steps;
+
+/// <summary>
+/// Test setup for Framework task scheduling.
+/// </summary>
+internal class FrameworkTaskSchedulerSelfTestStep : ISelfTestStep
+{
+    private bool passed = false;
+    private Task? task;
+
+    /// <inheritdoc/>
+    public string Name => "Test Framework Task Scheduler";
+
+    /// <inheritdoc/>
+    public SelfTestStepResult RunStep()
+    {
+        var framework = Service<Framework>.Get();
+
+        this.task ??= Task.Run(async () =>
+        {
+            ThreadSafety.AssertNotMainThread();
+
+            await framework.RunOnTick(async () =>
+            {
+                ThreadSafety.AssertMainThread();
+                await Task.Delay(100);
+
+                // TODO: We used to be scheduled back to the framework thread here. Is that by design?
+                //ThreadSafety.AssertMainThread();
+                ThreadSafety.AssertNotMainThread();
+            }).ConfigureAwait(false);
+
+            ThreadSafety.AssertNotMainThread();
+
+            await framework.RunOnTick(() =>
+            {
+                ThreadSafety.AssertMainThread();
+            });
+
+            ThreadSafety.AssertMainThread();
+
+            this.passed = true;
+        });
+
+        if (this.task is { IsFaulted: true } or { IsCanceled: true })
+        {
+            return SelfTestStepResult.Fail;
+        }
+
+        return this.passed ? SelfTestStepResult.Pass : SelfTestStepResult.Waiting;
+    }
+
+    /// <inheritdoc/>
+    public void CleanUp()
+    {
+        this.passed = false;
+        this.task = null;
+    }
+}
