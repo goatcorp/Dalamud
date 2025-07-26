e5d4d0ed1b2b8cf196dcb2d42293c3e40c5c9e35
MidoriKami <mi_kami@outlook.com>
2025-07-26 13:30:15 -0700

    Documentation fixing

--- Dalamud/Game/Addon/Events/EventDataTypes/AddonMouseEventData.cs
+++ Dalamud/Game/Addon/Events/EventDataTypes/AddonMouseEventData.cs
@@ -27,52 +27,52 @@ public unsafe class AddonMouseEventData : AddonEventData
     }
 
     /// <summary>
-    /// Gets a value indicating whether the event was a Left Mouse Click
+    /// Gets a value indicating whether the event was a Left Mouse Click.
     /// </summary>
     public bool IsLeftClick => this.MouseData.ButtonId is 0;
 
     /// <summary>
-    /// Gets a value indicating whether the event was a Right Mouse Click
+    /// Gets a value indicating whether the event was a Right Mouse Click.
     /// </summary>
     public bool IsRightClick => this.MouseData.ButtonId is 1;
 
     /// <summary>
-    /// Gets a value indicating whether there are any modifiers set such as alt, control, shift, or dragging
+    /// Gets a value indicating whether there are any modifiers set such as alt, control, shift, or dragging.
     /// </summary>
     public bool IsNoModifier => this.MouseData.Modifier is 0;
 
     /// <summary>
-    /// Gets a value indicating whether alt was being held when this event triggered
+    /// Gets a value indicating whether alt was being held when this event triggered.
     /// </summary>
     public bool IsAltHeld => this.MouseData.Modifier.HasFlag(ModifierFlag.Alt);
 
     /// <summary>
-    /// Gets a value indicating whether control was being held when this event triggered
+    /// Gets a value indicating whether control was being held when this event triggered.
     /// </summary>
     public bool IsControlHeld => this.MouseData.Modifier.HasFlag(ModifierFlag.Ctrl);
 
     /// <summary>
-    /// Gets a value indicating whether shift was being held when this event triggered
+    /// Gets a value indicating whether shift was being held when this event triggered.
     /// </summary>
     public bool IsShiftHeld => this.MouseData.Modifier.HasFlag(ModifierFlag.Shift);
 
     /// <summary>
-    /// Gets a value indicating whether this event is a mouse drag or not
+    /// Gets a value indicating whether this event is a mouse drag or not.
     /// </summary>
     public bool IsDragging => this.MouseData.Modifier.HasFlag(ModifierFlag.Dragging);
 
     /// <summary>
-    /// Gets a value indicating whether the event was a scroll up
+    /// Gets a value indicating whether the event was a scroll up.
     /// </summary>
     public bool IsScrollUp => this.MouseData.WheelDirection is 1;
 
     /// <summary>
-    /// Gets a value indicating whether the event was a scroll down
+    /// Gets a value indicating whether the event was a scroll down.
     /// </summary>
     public bool IsScrollDown => this.MouseData.WheelDirection is -1;
 
     /// <summary>
-    /// Gets the position of the mouse when this event was triggered
+    /// Gets the position of the mouse when this event was triggered.
     /// </summary>
     public Vector2 Position => new(this.MouseData.PosX, this.MouseData.PosY);
 
