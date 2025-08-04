c8a7c712c166e65f275e26e60c95e2df6fa6be16
Haselnussbomber <mail@haselnussbomber.de>
2025-08-04 21:03:38 +0200

    Use ImU8String for SafeTextColored

--- Dalamud/Interface/Utility/ImGuiHelpers.cs
+++ Dalamud/Interface/Utility/ImGuiHelpers.cs
@@ -246,16 +246,7 @@ public static partial class ImGuiHelpers
     /// </summary>
     /// <param name="color">The color of the text.</param>
     /// <param name="text">The text to write.</param>
-    public static void SafeTextColored(Vector4 color, string text)
-    {
-        using (ImRaii.PushColor(ImGuiCol.Text, color))
-        {
-            ImGui.TextUnformatted(text);
-        }
-    }
-
-    /// <inheritdoc cref="SafeTextColored(Vector4, string)"/>
-    public static void SafeTextColored(Vector4 color, ReadOnlySpan<byte> text)
+    public static void SafeTextColored(Vector4 color, ImU8String text)
     {
         using (ImRaii.PushColor(ImGuiCol.Text, color))
         {
