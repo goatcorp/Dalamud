7f2ed9adb6534934e6440b288fd6c753bddbe67c
Haselnussbomber <mail@haselnussbomber.de>
2025-11-13 19:01:04 +0100

    Convert Status to readonly struct and add interface

--- Dalamud/Game/ClientState/Statuses/Status.cs
+++ Dalamud/Game/ClientState/Statuses/Status.cs
@@ -1,61 +1,49 @@
+using System.Diagnostics.CodeAnalysis;
+
 using Dalamud.Data;
 using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.ClientState.Objects.Types;
 
 using Lumina.Excel;
 
+using CSStatus = FFXIVClientStructs.FFXIV.Client.Game.Status;
+
 namespace Dalamud.Game.ClientState.Statuses;
 
 /// <summary>
-/// This class represents a status effect an actor is afflicted by.
+/// Interface representing a status.
 /// </summary>
-public unsafe class Status
+public interface IStatus : IEquatable<IStatus>
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Status"/> class.
-    /// </summary>
-    /// <param name="address">Status address.</param>
-    internal Status(IntPtr address)
-    {
-        this.Address = address;
-    }
-
     /// <summary>
     /// Gets the address of the status in memory.
     /// </summary>
-    public IntPtr Address { get; }
+    nint Address { get; }
 
     /// <summary>
     /// Gets the status ID of this status.
     /// </summary>
-    public uint StatusId => this.Struct->StatusId;
+    uint StatusId { get; }
 
     /// <summary>
     /// Gets the GameData associated with this status.
     /// </summary>
-    public RowRef<Lumina.Excel.Sheets.Status> GameData => LuminaUtils.CreateRef<Lumina.Excel.Sheets.Status>(this.Struct->StatusId);
+    RowRef<Lumina.Excel.Sheets.Status> GameData { get; }
 
     /// <summary>
     /// Gets the parameter value of the status.
     /// </summary>
-    public ushort Param => this.Struct->Param;
-
-    /// <summary>
-    /// Gets the stack count of this status.
-    /// Only valid if this is a non-food status.
-    /// </summary>
-    [Obsolete($"Replaced with {nameof(Param)}", true)]
-    public byte StackCount => (byte)this.Struct->Param;
+    ushort Param { get; }
 
     /// <summary>
     /// Gets the time remaining of this status.
     /// </summary>
-    public float RemainingTime => this.Struct->RemainingTime;
+    float RemainingTime { get; }
 
     /// <summary>
     /// Gets the source ID of this status.
     /// </summary>
-    public uint SourceId => this.Struct->SourceObject.ObjectId;
+    uint SourceId { get; }
 
     /// <summary>
     /// Gets the source actor associated with this status.
@@ -63,7 +51,55 @@ public unsafe class Status
     /// <remarks>
     /// This iterates the actor table, it should be used with care.
     /// </remarks>
+    IGameObject? SourceObject { get; }
+}
+
+/// <summary>
+/// This struct represents a status effect an actor is afflicted by.
+/// </summary>
+/// <param name="ptr">A pointer to the Status.</param>
+internal unsafe readonly struct Status(CSStatus* ptr) : IStatus
+{
+    /// <inheritdoc/>
+    public nint Address => (nint)ptr;
+
+    /// <inheritdoc/>
+    public uint StatusId => ptr->StatusId;
+
+    /// <inheritdoc/>
+    public RowRef<Lumina.Excel.Sheets.Status> GameData => LuminaUtils.CreateRef<Lumina.Excel.Sheets.Status>(ptr->StatusId);
+
+    /// <inheritdoc/>
+    public ushort Param => ptr->Param;
+
+    /// <inheritdoc/>
+    public float RemainingTime => ptr->RemainingTime;
+
+    /// <inheritdoc/>
+    public uint SourceId => ptr->SourceObject.ObjectId;
+
+    /// <inheritdoc/>
     public IGameObject? SourceObject => Service<ObjectTable>.Get().SearchById(this.SourceId);
 
-    private FFXIVClientStructs.FFXIV.Client.Game.Status* Struct => (FFXIVClientStructs.FFXIV.Client.Game.Status*)this.Address;
+    public static bool operator ==(Status x, Status y) => x.Equals(y);
+
+    public static bool operator !=(Status x, Status y) => !(x == y);
+
+    /// <inheritdoc/>
+    public bool Equals(IStatus? other)
+    {
+        return this.StatusId == other.StatusId && this.SourceId == other.SourceId && this.Param == other.Param && this.RemainingTime == other.RemainingTime;
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        return obj is Status fate && this.Equals(fate);
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.StatusId, this.SourceId, this.Param, this.RemainingTime);
+    }
 }
