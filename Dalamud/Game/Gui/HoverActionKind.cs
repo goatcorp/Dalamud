2fc9884aad6217e61909f8b602a4782e7b29fd63
Haselnussbomber <mail@haselnussbomber.de>
2025-12-17 17:32:39 +0100

    Update HoverActionKind

--- Dalamud/Game/Gui/HoverActionKind.cs
+++ Dalamud/Game/Gui/HoverActionKind.cs
@@ -14,140 +14,145 @@ public enum HoverActionKind
     /// <summary>
     /// A regular action is hovered.
     /// </summary>
-    Action = 28,
+    Action = 29,
 
     /// <summary>
     /// A crafting action is hovered.
     /// </summary>
-    CraftingAction = 29,
+    CraftingAction = 30,
 
     /// <summary>
     /// A general action is hovered.
     /// </summary>
-    GeneralAction = 30,
+    GeneralAction = 31,
 
     /// <summary>
     /// A companion order type of action is hovered.
     /// </summary>
-    CompanionOrder = 31, // Game Term: BuddyOrder
+    CompanionOrder = 32, // Game Term: BuddyOrder
 
     /// <summary>
     /// A main command type of action is hovered.
     /// </summary>
-    MainCommand = 32,
+    MainCommand = 33,
 
     /// <summary>
     /// An extras command type of action is hovered.
     /// </summary>
-    ExtraCommand = 33,
+    ExtraCommand = 34,
 
     /// <summary>
     /// A companion action is hovered.
     /// </summary>
-    Companion = 34,
+    Companion = 35,
 
     /// <summary>
     /// A pet order type of action is hovered.
     /// </summary>
-    PetOrder = 35,
+    PetOrder = 36,
 
     /// <summary>
     /// A trait is hovered.
     /// </summary>
-    Trait = 36,
+    Trait = 37,
 
     /// <summary>
     /// A buddy action is hovered.
     /// </summary>
-    BuddyAction = 37,
+    BuddyAction = 38,
 
     /// <summary>
     /// A company action is hovered.
     /// </summary>
-    CompanyAction = 38,
+    CompanyAction = 39,
 
     /// <summary>
     /// A mount is hovered.
     /// </summary>
-    Mount = 39,
+    Mount = 40,
 
     /// <summary>
     /// A chocobo race action is hovered.
     /// </summary>
-    ChocoboRaceAction = 40,
+    ChocoboRaceAction = 41,
 
     /// <summary>
     /// A chocobo race item is hovered.
     /// </summary>
-    ChocoboRaceItem = 41,
+    ChocoboRaceItem = 42,
 
     /// <summary>
     /// A deep dungeon equipment is hovered.
     /// </summary>
-    DeepDungeonEquipment = 42,
+    DeepDungeonEquipment = 43,
 
     /// <summary>
     /// A deep dungeon equipment 2 is hovered.
     /// </summary>
-    DeepDungeonEquipment2 = 43,
+    DeepDungeonEquipment2 = 44,
 
     /// <summary>
     /// A deep dungeon item is hovered.
     /// </summary>
-    DeepDungeonItem = 44,
+    DeepDungeonItem = 45,
 
     /// <summary>
     /// A quick chat is hovered.
     /// </summary>
-    QuickChat = 45,
+    QuickChat = 46,
 
     /// <summary>
     /// An action combo route is hovered.
     /// </summary>
-    ActionComboRoute = 46,
+    ActionComboRoute = 47,
 
     /// <summary>
     /// A pvp trait is hovered.
     /// </summary>
-    PvPSelectTrait = 47,
+    PvPSelectTrait = 48,
 
     /// <summary>
     /// A squadron action is hovered.
     /// </summary>
-    BgcArmyAction = 48,
+    BgcArmyAction = 49,
 
     /// <summary>
     /// A perform action is hovered.
     /// </summary>
-    Perform = 49,
+    Perform = 50,
 
     /// <summary>
     /// A deep dungeon magic stone is hovered.
     /// </summary>
-    DeepDungeonMagicStone = 50,
+    DeepDungeonMagicStone = 51,
 
     /// <summary>
     /// A deep dungeon demiclone is hovered.
     /// </summary>
-    DeepDungeonDemiclone = 51,
+    DeepDungeonDemiclone = 52,
 
     /// <summary>
     /// An eureka magia action is hovered.
     /// </summary>
-    EurekaMagiaAction = 52,
+    EurekaMagiaAction = 53,
 
     /// <summary>
     /// An island sanctuary temporary item is hovered.
     /// </summary>
-    MYCTemporaryItem = 53,
+    MYCTemporaryItem = 54,
 
     /// <summary>
     /// An ornament is hovered.
     /// </summary>
-    Ornament = 54,
+    Ornament = 55,
 
     /// <summary>
     /// Glasses are hovered.
     /// </summary>
-    Glasses = 55,
+    Glasses = 56,
+
+    /// <summary>
+    /// Phantom Job Trait is hovered.
+    /// </summary>
+    MKDTrait = 58,
 }
