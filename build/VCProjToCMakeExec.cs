3ecc3a00fc985d633f44827b22f4e3276e3b9e0b
Jade Macho <ade@0x0a.de>
2026-03-19 18:31:14 +0100

    mingw: First steps towards building via autogenned CMakeLists

---
+++ build/VCProjToCMakeExec.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities;
+
+public sealed class VCProjToCMakeExec(ICMakePaths paths) : IVCProjExec
+{
+    readonly ICMakePaths Paths = paths;
+
+    public IReadOnlyCollection<Output> Build(AbsolutePath path, Configuration config)
+    {
+        var cmakelists = new VCProjToCMakeLists(Paths, path, config);
+        var dir = cmakelists.Gen();
+
+        List<Output> outputs = [];
+
+        outputs.AddRange(Paths.CMake(
+                $"-S{dir.ToString().DoubleQuoteIfNeeded()} " +
+                $"-B{dir.ToString().DoubleQuoteIfNeeded()}"));
+
+        outputs.AddRange(Paths.CMake(
+            $"--build {dir.ToString().DoubleQuoteIfNeeded()}"));
+
+        return outputs;
+    }
+
+    public IReadOnlyCollection<Output> Clean(AbsolutePath path, Configuration config)
+    {
+        var cmakelists = new VCProjToCMakeLists(Paths, path, config);
+        cmakelists.Clean();
+
+        return [];
+    }
+}
