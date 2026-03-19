63f8e5de9e2cf1cdb139743508a68d33b73496a8
Jade Macho <ade@0x0a.de>
2026-03-19 18:31:14 +0100

    mingw: First steps towards building via autogenned CMakeLists

---
+++ build/CMakePaths.cs
@@ -0,0 +1,38 @@
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+
+public interface ICMakePaths
+{
+    Tool CMake { get; }
+
+    AbsolutePath BuildToolDirectory { get; }
+
+    AbsolutePath CMakeToolchain { get; }
+
+    AbsolutePath VCStubDirectory { get; }
+
+    AbsolutePath ProjectsDirectory { get; }
+
+    AbsolutePath JWasmSrcDirectory { get; }
+
+    AbsolutePath JWasmBuildDirectory { get; }
+
+    AbsolutePath JWasmTool { get; }
+}
+
+public sealed record DefaultCMakePaths(AbsolutePath RootDirectory, Tool CMake) : ICMakePaths
+{
+    public AbsolutePath BuildToolDirectory => RootDirectory / "build";
+
+    public AbsolutePath CMakeToolchain => BuildToolDirectory / "CMakeMinGW.cmake";
+
+    public AbsolutePath VCStubDirectory => BuildToolDirectory / "vcstub";
+
+    public AbsolutePath ProjectsDirectory => BuildToolDirectory / "cmake";
+
+    public AbsolutePath JWasmSrcDirectory => RootDirectory / "lib" / "JWasm";
+
+    public AbsolutePath JWasmBuildDirectory => JWasmSrcDirectory / "build";
+
+    public AbsolutePath JWasmTool => JWasmBuildDirectory / "jwasm";
+}
