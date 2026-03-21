a3eae9a1fafd25fd6c60e2a2585a85fcf3fb7ba9
Jade Macho <ade@0x0a.de>
2026-03-21 20:34:31 +0100

    mingw: Fix wine dotnet test wrapper not unsetting DOTNET_ROOT

--- build/wine-dotnet-test.sh
+++ build/wine-dotnet-test.sh
@@ -3,10 +3,12 @@
 
 export WINEDEBUG=-all
 export DOTNET_NOLOGO=true
+export DOTNET_ROOT=null
+export DOTNET_ROOT_X64=null
 
 if [ "$1" != "test" ] || [ ! -f "$2" ]; then
     echo "Must run as test with file"
     exit 1
 fi
 
-exec wine dotnet "$1" "$(winepath -w -- "$2")" ${@:3}
+exec wine dotnet "$1" "$(winepath -w -- "$2")" "${@:3}"
