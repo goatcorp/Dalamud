fec8b27506d9ba621924d06e328a50e4f24ab371
Haselnussbomber <mail@haselnussbomber.de>
2026-03-23 15:50:48 +0100

    Avoid using service locator in HandleFrameworkUpdate

--- Dalamud/Game/Framework.cs
+++ Dalamud/Game/Framework.cs
@@ -374,26 +374,17 @@ internal sealed class Framework : IInternalDisposableService, IFramework
 
         ThreadSafety.MarkMainThread();
 
-        this.BeforeUpdate?.InvokeSafely(this);
-
         this.hitchDetector.Start();
+
+        this.BeforeUpdate?.InvokeSafely(this);
 
         try
         {
-            var chatGui = Service<ChatGui>.GetNullable();
-            var toastGui = Service<ToastGui>.GetNullable();
-            var config = Service<DalamudConfiguration>.GetNullable();
-            if (chatGui == null || toastGui == null)
-                goto original;
-
-            chatGui.UpdateQueue();
-            toastGui.UpdateQueue();
-
-            config?.Update();
+            this.configuration.Update();
         }
         catch (Exception ex)
         {
-            Log.Error(ex, "Exception while handling Framework::Update hook.");
+            Log.Error(ex, "Exception in DalamudConfiguration.Update.");
         }
 
         if (this.DispatchUpdateEvents)
@@ -460,7 +451,6 @@ internal sealed class Framework : IInternalDisposableService, IFramework
 
         this.hitchDetector.Stop();
 
-    original:
         return this.updateHook.OriginalDisposeSafe(thisPtr);
     }
 
