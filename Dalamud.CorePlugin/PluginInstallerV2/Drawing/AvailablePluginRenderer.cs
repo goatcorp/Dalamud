0b1166316448990aa7d1bd33c7fd053dfac58397
MidoriKami <mi_kami@outlook.com>
2026-04-04 17:42:22 -0700

    Reduce nesting a bit, not sure if I like this better or not.

--- Dalamud.CorePlugin/PluginInstallerV2/Drawing/AvailablePluginRenderer.cs
+++ Dalamud.CorePlugin/PluginInstallerV2/Drawing/AvailablePluginRenderer.cs
@@ -63,86 +63,108 @@ internal class AvailablePluginRenderer : PluginEntryRenderer
 
         ImGui.SameLine();
 
-        using (var contentsChild = ImRaii.Child("ContentsChild", ImGui.GetContentRegionAvail(), false, ImGuiWindowFlags.NoInputs))
-        {
-            if (contentsChild.Success)
-            {
-                var downloadSize = 150.0f * ImGuiHelpers.GlobalScale;
-                var repositorySize = 150.0f * ImGuiHelpers.GlobalScale;
+        this.DrawContents(manifest);
+    }
 
-                using (var titleChild = ImRaii.Child($"TitleChild", new Vector2(ImGui.GetContentRegionAvail().X - downloadSize - repositorySize - (ImGui.GetStyle().ItemSpacing.X * 2.0f), ImGui.GetContentRegionAvail().Y * (3.0f / 5.0f)), false, ImGuiWindowFlags.NoInputs))
+    private static void DrawExtras(RemotePluginManifest manifest, float downloadSize, float repositorySize)
+    {
+        using var extrasChild = ImRaii.Child($"ExtrasChild", new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetContentRegionAvail().Y * (3.0f / 5.0f)), false, ImGuiWindowFlags.NoInputs);
+        if (!extrasChild.Success)
+        {
+            return;
+        }
+
+        using (var topLineChild = ImRaii.Child("TopLineChild", new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetContentRegionAvail().Y / 2.0f), false, ImGuiWindowFlags.NoInputs))
+        {
+            if (topLineChild.Success)
+            {
+                using (var downloadsChild = ImRaii.Child("DownloadsChild", new Vector2(downloadSize, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs))
                 {
-                    if (titleChild.Success)
+                    if (downloadsChild.Success)
                     {
-                        this.DrawPluginTitle(manifest);
+                        DrawPluginDownloadCount(manifest);
                     }
                 }
 
                 ImGui.SameLine();
 
-                using (var extrasChild = ImRaii.Child($"ExtrasChild", new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetContentRegionAvail().Y * (3.0f / 5.0f)), false, ImGuiWindowFlags.NoInputs))
+                using (var repoChild = ImRaii.Child("RepositoryChild", new Vector2(repositorySize, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs))
                 {
-                    if (extrasChild.Success)
+                    if (repoChild.Success)
                     {
-                        using (var topLineChild = ImRaii.Child("TopLineChild", new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetContentRegionAvail().Y / 2.0f), false, ImGuiWindowFlags.NoInputs))
-                        {
-                            if (topLineChild.Success)
-                            {
-                                using (var downloadsChild = ImRaii.Child("DownloadsChild", new Vector2(downloadSize, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs))
-                                {
-                                    if (downloadsChild.Success)
-                                    {
-                                        DrawPluginDownloadCount(manifest);
-                                    }
-                                }
-
-                                ImGui.SameLine();
-
-                                using (var repoChild = ImRaii.Child("RepositoryChild", new Vector2(repositorySize, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs))
-                                {
-                                    if (repoChild.Success)
-                                    {
-                                        DrawRepoSource(manifest);
-                                    }
-                                }
-                            }
-                        }
-
-                        using (var bottomLineChild = ImRaii.Child("BottomLineChild", new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs))
-                        {
-                            if (bottomLineChild.Success)
-                            {
-                                // Placeholder for Badges like AI Generated, etc.
-                            }
-                        }
-                    }
-                }
-
-                using (var footerChild = ImRaii.Child("FooterChild", ImGui.GetContentRegionAvail(), false, ImGuiWindowFlags.NoInputs))
-                {
-                    if (footerChild.Success)
-                    {
-                        var controlWidgetSize = 100.0f * ImGuiHelpers.GlobalScale;
-
-                        using (var punchlineChild = ImRaii.Child("PunchlineChild", new Vector2(ImGui.GetContentRegionAvail().X - ImGui.GetStyle().ItemSpacing.X - controlWidgetSize, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
-                        {
-                            if (punchlineChild.Success)
-                            {
-                                DrawPunchline(manifest);
-                            }
-                        }
-
-                        ImGui.SameLine();
-
-                        using (var widgetChild = ImRaii.Child("WidgetChild", ImGui.GetContentRegionAvail(), false, ImGuiWindowFlags.NoInputs))
-                        {
-                            if (widgetChild.Success)
-                            {
-                            }
-                        }
+                        DrawRepoSource(manifest);
                     }
                 }
             }
         }
+
+        using (var bottomLineChild = ImRaii.Child("BottomLineChild", new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs))
+        {
+            if (bottomLineChild.Success)
+            {
+                // Placeholder for Badges like AI Generated, etc.
+            }
+        }
+    }
+
+    private static void DrawFooter(RemotePluginManifest manifest)
+    {
+        using var footerChild = ImRaii.Child("FooterChild", ImGui.GetContentRegionAvail(), false, ImGuiWindowFlags.NoInputs);
+        if (!footerChild.Success)
+        {
+            return;
+        }
+
+        var controlWidgetSize = 100.0f * ImGuiHelpers.GlobalScale;
+
+        using (var punchlineChild = ImRaii.Child("PunchlineChild", new Vector2(ImGui.GetContentRegionAvail().X - ImGui.GetStyle().ItemSpacing.X - controlWidgetSize, ImGui.GetContentRegionAvail().Y), false, ImGuiWindowFlags.NoInputs | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+        {
+            if (punchlineChild.Success)
+            {
+                DrawPunchline(manifest);
+            }
+        }
+
+        ImGui.SameLine();
+
+        using (var widgetChild = ImRaii.Child("WidgetChild", ImGui.GetContentRegionAvail(), false, ImGuiWindowFlags.NoInputs))
+        {
+            if (widgetChild.Success)
+            {
+                // Placeholder for Enable/Disable Widget
+            }
+        }
+    }
+
+    /// <summary>
+    /// Draws plugins main content body.
+    /// </summary>
+    /// <param name="manifest">Manifest.</param>
+    private void DrawContents(RemotePluginManifest manifest)
+    {
+        using var contentsChild = ImRaii.Child("ContentsChild", ImGui.GetContentRegionAvail(), false, ImGuiWindowFlags.NoInputs);
+        if (!contentsChild.Success)
+        {
+            return;
+        }
+
+        var downloadSize = 150.0f * ImGuiHelpers.GlobalScale;
+        var repositorySize = 150.0f * ImGuiHelpers.GlobalScale;
+
+        using (var titleChild = ImRaii.Child($"TitleChild", new Vector2(ImGui.GetContentRegionAvail().X - downloadSize - repositorySize - (ImGui.GetStyle().ItemSpacing.X * 2.0f), ImGui.GetContentRegionAvail().Y * (3.0f / 5.0f)), false, ImGuiWindowFlags.NoInputs))
+        {
+            if (titleChild.Success)
+            {
+                this.DrawPluginTitle(manifest);
+            }
+        }
+
+        ImGui.SameLine();
+
+        // Extras are Download Count, Source Repository, and maybe in the future some badge icons.
+        DrawExtras(manifest, downloadSize, repositorySize);
+
+        // Footer is Punchline and a toggle button to enable/disable.
+        DrawFooter(manifest);
     }
 }
