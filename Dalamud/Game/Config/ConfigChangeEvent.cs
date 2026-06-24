c7ebdd2c302337af9f3d048c273abd1f56ff3cda
Haselnussbomber <mail@haselnussbomber.de>
2026-06-24 04:39:13 +0200

    Add explicit constructors to avoid API break

--- Dalamud/Game/Config/ConfigChangeEvent.cs
+++ Dalamud/Game/Config/ConfigChangeEvent.cs
@@ -3,14 +3,54 @@ namespace Dalamud.Game.Config;
 /// <summary>
 /// Represents a change in the configuration.
 /// </summary>
-/// <param name="Option">The option that was changed.</param>
-/// <param name="Name">The name of the option that was changed.</param>
-public abstract record ConfigChangeEvent(Enum Option, string Name);
+public abstract record ConfigChangeEvent
+{
+    /// <summary> Initializes a new instance of the <see cref="ConfigChangeEvent"/> class. </summary>
+    /// <param name="option">The option that was changed.</param>
+    public ConfigChangeEvent(Enum option)
+    {
+        this.Option = option;
+        this.Name = string.Empty;
+    }
+
+    /// <summary> Initializes a new instance of the <see cref="ConfigChangeEvent"/> class. </summary>
+    /// <param name="option">The option that was changed.</param>
+    /// <param name="name">The name of the option that was changed.</param>
+    public ConfigChangeEvent(Enum option, string name)
+    {
+        this.Option = option;
+        this.Name = name;
+    }
+
+    /// <summary>
+    /// Gets the option that was changed.
+    /// </summary>
+    public Enum Option { get; init; }
+
+    /// <summary>
+    /// Gets the name of the option that was changed.
+    /// </summary>
+    public string Name { get; init; }
+}
 
 /// <summary>
 /// Represents a generic change in the configuration.
 /// </summary>
-/// <param name="ConfigOption">The option that was changed.</param>
-/// <param name="Name">The name of the option that was changed.</param>
 /// <typeparam name="T">The type of the option.</typeparam>
-public record ConfigChangeEvent<T>(T ConfigOption, string Name) : ConfigChangeEvent(ConfigOption, Name) where T : Enum;
+public record ConfigChangeEvent<T> : ConfigChangeEvent where T : Enum
+{
+    /// <summary> Initializes a new instance of the <see cref="ConfigChangeEvent{T}"/> class. </summary>
+    /// <param name="option">The option that was changed.</param>
+    public ConfigChangeEvent(T option)
+        : base(option)
+    {
+    }
+
+    /// <summary> Initializes a new instance of the <see cref="ConfigChangeEvent{T}"/> class. </summary>
+    /// <param name="option">The option that was changed.</param>
+    /// <param name="name">The name of the option that was changed.</param>
+    public ConfigChangeEvent(T option, string name)
+        : base(option, name)
+    {
+    }
+}
