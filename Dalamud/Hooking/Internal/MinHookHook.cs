goatcorp/Dalamud

commit 2c735e9ec3de0c8019a60cb593631d5142ff8db4

Do not throw ObjectDisposedException in IsEnabled and Disable (#2266)

Dalamud/Hooking/Internal/MinHookHook.cs
@@ -50,14 +50,7 @@ internal class MinHookHook<T> : Hook<T> where T : Delegate
     }
 
     /// <inheritdoc/>
-    public override bool IsEnabled
-    {
-        get
-        {
-            this.CheckDisposed();
-            return this.minHookImpl.Enabled;
-        }
-    }
+    public override bool IsEnabled => !this.IsDisposed && this.minHookImpl.Enabled;
 
     /// <inheritdoc/>
     public override string BackendName => "MinHook";
@@ -84,28 +77,29 @@ internal class MinHookHook<T> : Hook<T> where T : Delegate
     /// <inheritdoc/>
     public override void Enable()
     {
-        this.CheckDisposed();
-
-        if (!this.minHookImpl.Enabled)
+        lock (HookManager.HookEnableSyncRoot)
         {
-            lock (HookManager.HookEnableSyncRoot)
-            {
-                this.minHookImpl.Enable();
-            }
+            this.CheckDisposed();
+
+            if (!this.minHookImpl.Enabled)
+                return;
+
+            this.minHookImpl.Enable();
         }
     }
 
     /// <inheritdoc/>
     public override void Disable()
     {
-        this.CheckDisposed();
-
-        if (this.minHookImpl.Enabled)
+        lock (HookManager.HookEnableSyncRoot)
         {
-            lock (HookManager.HookEnableSyncRoot)
-            {
-                this.minHookImpl.Disable();
-            }
+            if (this.IsDisposed)
+                return;
+
+            if (!this.minHookImpl.Enabled)
+                return;
+
+            this.minHookImpl.Disable();
         }
     }
 }

Dalamud/Hooking/Hook.cs
@@ -90,6 +90,7 @@ public abstract class Hook<T> : IDalamudHook where T : Delegate
     /// <summary>
     /// Starts intercepting a call to the function.
     /// </summary>
+    /// <exception cref="ObjectDisposedException">Hook is already disposed.</exception>
     public virtual void Enable() => throw new NotImplementedException();
 
     /// <summary>

Dalamud/Hooking/Internal/FunctionPointerVariableHook.cs
@@ -115,14 +115,7 @@ internal class FunctionPointerVariableHook<T> : Hook<T>
     }
 
     /// <inheritdoc/>
-    public override bool IsEnabled
-    {
-        get
-        {
-            this.CheckDisposed();
-            return this.enabled;
-        }
-    }
+    public override bool IsEnabled => !this.IsDisposed && this.enabled;
 
     /// <inheritdoc/>
     public override string BackendName => "MinHook";
@@ -131,9 +124,7 @@ internal class FunctionPointerVariableHook<T> : Hook<T>
     public override void Dispose()
     {
         if (this.IsDisposed)
-        {
             return;
-        }
 
         this.Disable();
 
@@ -148,15 +139,13 @@ internal class FunctionPointerVariableHook<T> : Hook<T>
     /// <inheritdoc/>
     public override void Enable()
     {
-        this.CheckDisposed();
-
-        if (this.enabled)
-        {
-            return;
-        }
-
         lock (HookManager.HookEnableSyncRoot)
         {
+            this.CheckDisposed();
+
+            if (this.enabled)
+                return;
+
             Marshal.WriteIntPtr(this.ppfnThunkJumpTarget, this.pfnDetour);
             this.enabled = true;
         }
@@ -165,15 +154,14 @@ internal class FunctionPointerVariableHook<T> : Hook<T>
     /// <inheritdoc/>
     public override void Disable()
     {
-        this.CheckDisposed();
-
-        if (!this.enabled)
-        {
-            return;
-        }
-
         lock (HookManager.HookEnableSyncRoot)
         {
+            if (this.IsDisposed)
+                return;
+
+            if (!this.enabled)
+                return;
+
             Marshal.WriteIntPtr(this.ppfnThunkJumpTarget, this.pfnOriginal);
             this.enabled = false;
         }

Dalamud/Hooking/Internal/ReloadedHook.cs
@@ -45,14 +45,7 @@ internal class ReloadedHook<T> : Hook<T> where T : Delegate
     }
 
     /// <inheritdoc/>
-    public override bool IsEnabled
-    {
-        get
-        {
-            this.CheckDisposed();
-            return this.hookImpl.IsHookEnabled;
-        }
-    }
+    public override bool IsEnabled => !this.IsDisposed && this.hookImpl.IsHookEnabled;
 
     /// <inheritdoc/>
     public override string BackendName => "Reloaded";
@@ -73,10 +66,10 @@ internal class ReloadedHook<T> : Hook<T> where T : Delegate
     /// <inheritdoc/>
     public override void Enable()
     {
-        this.CheckDisposed();
-
         lock (HookManager.HookEnableSyncRoot)
         {
+            this.CheckDisposed();
+
             if (!this.hookImpl.IsHookEnabled)
                 this.hookImpl.Enable();
         }
@@ -85,10 +78,11 @@ internal class ReloadedHook<T> : Hook<T> where T : Delegate
     /// <inheritdoc/>
     public override void Disable()
     {
-        this.CheckDisposed();
-
         lock (HookManager.HookEnableSyncRoot)
         {
+            if (this.IsDisposed)
+                return;
+
             if (!this.hookImpl.IsHookActivated)
                 return;
 

commit c3d97e0ffaf1a96c4948b4f53dee688fe3d6d173

Fix MinHook enabling
I wanted to test MinHook on Linux, and found that it didn't work. It
looks like it regressed in 2c735e9ec on May 9, 2025.

Dalamud/Hooking/Internal/MinHookHook.cs
@@ -80,9 +80,7 @@ internal class MinHookHook<T> : Hook<T> where T : Delegate
         this.CheckDisposed();
 
         if (!this.minHookImpl.Enabled)
-            return;
-
-        this.minHookImpl.Enable();
+            this.minHookImpl.Enable();
     }
 
     /// <inheritdoc/>