38e65221bfd15cce579e998c8ee24511c89280bb
Haselnussbomber <mail@haselnussbomber.de>
2026-09-21 19:01:08 +0200

    Opt out of NUKE telemetry

--- build.ps1
+++ build.ps1
@@ -20,6 +20,7 @@ $DotNetGlobalFile = "$PSScriptRoot\\global.json"
 $DotNetInstallUrl = "https://dot.net/v1/dotnet-install.ps1"
 $DotNetChannel = "Current"
 
+$env:NUKE_TELEMETRY_OPTOUT = 1
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 $env:DOTNET_MULTILEVEL_LOOKUP = 0
